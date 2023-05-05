089231b568957fa8404dfb4f60dd9eb68b1356fc
rafaqz <rafaelschouten@gmail.com>
2023-05-05 21:21:13 +0200

    test new primitives

--- test/primitives.jl
+++ test/primitives.jl
@@ -3,12 +3,40 @@ using Test
 import GeoInterface as GI
 import GeometryOps as GO
 
-geom = GI.Polygon([GI.LinearRing([(1, 2), (3, 4), (5, 6), (1, 2)]), 
-                   GI.LinearRing([(3, 4), (5, 6), (6, 7), (3, 4)])])
+pv1 = [(2, 1), (4, 3), (6, 5), (2, 1)]
+pv2 = [(4, 3), (6, 5), (7, 6), (4, 3)]
+lr1 = GI.LinearRing(pv1)
+lr2 =  GI.LinearRing(pv2)
+poly = GI.Polygon([lr1, lr2])
 
-flipped_geom = GO.apply(GI.PointTrait, geom) do p
-    (GI.y(p), GI.x(p))
+@testset "apply" begin
+
+    flipped_poly = GO.apply(GI.PointTrait, poly) do p
+        (GI.y(p), GI.x(p))
+    end
+
+    @test flipped_poly == GI.Polygon([GI.LinearRing([(2, 1), (4, 3), (6, 5), (2, 1)]), 
+                                      GI.LinearRing([(4, 3), (6, 5), (7, 6), (4, 3)])])
 end
 
-@test flipped_geom == GI.Polygon([GI.LinearRing([(2, 1), (4, 3), (6, 5), (2, 1)]), 
-                                  GI.LinearRing([(4, 3), (6, 5), (7, 6), (4, 3)])])
+@testset "unwrap" begin
+    flipped_vectors = GO.unwrap(GI.PointTrait, poly) do p
+        (GI.y(p), GI.x(p))
+    end
+
+    @test flipped_vectors == [[(2, 1), (4, 3), (6, 5), (2, 1)], [(4, 3), (6, 5), (7, 6), (4, 3)]]
+end
+
+@testset "flatten" begin
+    very_wrapped = [[GI.FeatureCollection([GI.Feature(poly; properties=(;))])]]
+    @test collect(GO.flatten(GI.PointTrait, very_wrapped)) == vcat(pv1, pv2)
+    @test collect(GO.flatten(GI.LinearRingTrait, [poly])) == [lr1, lr2]
+end
+
+@testset "reconstruct" begin
+    revlr1 =  GI.LinearRing(reverse(pv2))
+    revlr2 = GI.LinearRing(reverse(pv1))
+    revpoly = GI.Polygon([revlr1, revlr2])
+    points = collect(GO.flatten(GI.PointTrait, poly))
+    @test GO.reconstruct(poly, reverse(points)) == revpoly
+end
