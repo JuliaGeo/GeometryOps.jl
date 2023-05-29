0a661e1fa4a1a804533f755a69b00e75513d9407
rafaqz <rafaelschouten@gmail.com>
2023-05-29 10:58:59 +0200

    get most bools working

--- src/methods/bools.jl
+++ src/methods/bools.jl
@@ -8,14 +8,16 @@ Take a ring and return true or false whether or not the ring is clockwise or cou
 
 # Examples
 ```jldoctest
-julia> line = LineString([[0, 0], [1, 1], [1, 0], [0, 0]])
-LineString(Array{Float64,1}[[0.0, 0.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]])
+import GeoInterface as GI, GeometryOps as GO
+julia> 
+line = GI.LineString([(0, 0), (1, 1), (1, 0), (0, 0)])
+GeoInterface.Wrappers.LineString{false, false, Vector{Tuple{Int64, Int64}}, Nothing, Nothing}([(0, 0), (1, 1), (1, 0), (0, 0)], nothing, nothing)
 
-julia> clockwise(line)
+julia> GO.isclockwise(line)
 true
 ```
 """
-isclockwise(geom)::Bool isclockwise(GI.trait(line), geom)
+isclockwise(geom)::Bool = isclockwise(GI.trait(geom), geom)
 function isclockwise(::AbstractCurveTrait, line)::Bool
     sum = 0.0
     prev = GI.getpoint(line, 1)
@@ -35,10 +37,11 @@ Take a polygon and return true or false as to whether it is concave or not.
 
 # Examples
 ```jldoctest
-julia> poly = Polygon([[[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]]])
+import GeoInterface as GI, GeometryOps as GO
+julia> poly = GI.Polygon([[(0, 0), (0, 1), (1, 1), (1, 0), (0, 0)]])
 Polygon(Array{Array{Float64,1},1}[[[0.0, 0.0], [0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]]])
 
-julia> concave(poly)
+julia> GO.isconcave(poly)
 false
 ```
 """
@@ -54,9 +57,9 @@ function isconcave(poly)::Bool
         j = ((i + 1) % n) === 0 ? 1 : (i + 1) % n
         m = ((i + 2) % n) === 0 ? 1 : (i + 2) % n
 
-        pti = GI.gepoint(exterior, i)
-        ptj = GI.gepoint(exterior, j)
-        ptm = GI.gepoint(exterior, m)
+        pti = GI.getpoint(exterior, i)
+        ptj = GI.getpoint(exterior, j)
+        ptm = GI.getpoint(exterior, m)
 
         dx1 = GI.x(ptm) - GI.x(ptj)
         dy1 = GI.y(ptm) - GI.y(ptj)
@@ -100,43 +103,44 @@ function compare_lines(p1::Vector, p2::Vector)
     length(p1[1]) !== length(p2[1]) && return false
 end
 
-"""
-    parallel(line1::LineString, line2::LineString)::Bool
+# """
+#     parallel(line1::LineString, line2::LineString)::Bool
 
-Return `true` if each segment of `line1` is parallel to the correspondent segment of `line2`
+# Return `true` if each segment of `line1` is parallel to the correspondent segment of `line2`
 
-# Examples
-```jldoctest
-julia> line1 = LineString([[9.170356, 45.477985], [9.164434, 45.482551], [9.166644, 45.484003]])
-LineString(Array{Float64,1}[[9.17036, 45.478], [9.16443, 45.4826], [9.16664, 45.484]])
+# # Examples
+# ```jldoctest
+# import GeoInterface as GI, GeometryOps as GO
+# julia> line1 = GI.LineString([(9.170356, 45.477985), (9.164434, 45.482551), (9.166644, 45.484003)])
+# GeoInterface.Wrappers.LineString{false, false, Vector{Tuple{Float64, Float64}}, Nothing, Nothing}([(9.170356, 45.477985), (9.164434, 45.482551), (9.166644, 45.484003)], nothing, nothing)
 
-julia> line2 = LineString([[9.169356, 45.477985], [9.163434, 45.482551], [9.165644, 45.484003]])
-LineString(Array{Float64,1}[[9.16936, 45.478], [9.16343, 45.4826], [9.16564, 45.484]])
+# julia> line2 = GI.LineString([(9.169356, 45.477985), (9.163434, 45.482551), (9.165644, 45.484003)])
+# GeoInterface.Wrappers.LineString{false, false, Vector{Tuple{Float64, Float64}}, Nothing, Nothing}([(9.169356, 45.477985), (9.163434, 45.482551), (9.165644, 45.484003)], nothing, nothing)
 
-julia> parallel(line1, line2)
-true
-```
-"""
-function parallel(line1, line2)::Bool
-    seg1 = linesegment(line1)
-    seg2 = linesegment(line2)
+# julia> 
+# GO.isparallel(line1, line2)
+# true
+# ```
+# """
+# function isparallel(line1, line2)::Bool
+#     seg1 = linesegment(line1)
+#     seg2 = linesegment(line2)
 
-    for i in eachindex(seg1)
-        coors2 = nothing
-        coors1 = seg1[i]
-        coors2 = seg2i]
-        _isparallel(coors1, coors2) == false && return false
-    end
+#     for i in eachindex(seg1)
+#         coors2 = nothing
+#         coors1 = seg1[i]
+#         coors2 = seg2[i]
+#         _isparallel(coors1, coors2) == false && return false
+#     end
+#     return true
+# end
 
-    return true
-end
+# @inline function _isparallel(p1, p2)
+#     slope1 = bearing_to_azimuth(rhumb_bearing(GI.x(p1), GI.x(p2)))
+#     slope2 = bearing_to_azimuth(rhumb_bearing(GI.y(p1), GI.y(p2)))
 
-@inline function _isparallel(p1, p2)
-    slope1 = bearing_to_azimuth(rhumb_bearing(GI.x(p1), GI.x(p2)))
-    slope2 = bearing_to_azimuth(rhumb_bearing(GI.y(p1), GI.y(p2)))
-
-    return slope1 === slope2
-end
+#     return slope1 === slope2
+# end
 
 
 """
@@ -147,34 +151,41 @@ start and end vertices of the linestring.
 
 # Examples
 ```jldoctest
-julia> point = Point([1,1])
-Point([1.0, 1.0])
+import GeoInterface as GI, GeometryOps as GO
+julia> point = GI.Point(1, 1)
+GeoInterface.Wrappers.Point{false, false, Tuple{Int64, Int64}, Nothing}((1, 1), nothing)
 
-julia> line = LineString([[0, 0], [3, 3], [4, 4]])
-LineString(Array{Float64,1}[[0.0, 0.0], [3.0, 3.0], [4.0, 4.0]])
+julia> line = GI.LineString([(0, 0), (3, 3), (4, 4)])
+GeoInterface.Wrappers.LineString{false, false, Vector{Tuple{Int64, Int64}}, Nothing, Nothing}([(0, 0), (3, 3), (4, 4)], nothing, nothing)
 
-julia> point_on_line(point, line)
+julia> GO.point_on_line(point, line)
 true
 ```
 """
 function point_on_line(point, line; ignore_end_vertices::Bool=false)::Bool
-    pCoords = point.coordinates
-    lCoords = line.coordinates
+    line_points = tuple_points(line)
+    n = length(line_points)
 
-    ignore = "none"
-    for i in 1:length(lCoords) - 1
+    ignore = :none
+    for i in 1:n - 1
         if ignore_end_vertices == true
-            i === 1 && (ignore = "start")
-            i === length(lCoords) - 2 && (ignore = "end")
-            (i === 1 && i + 1 === length(lCoords) - 1) && (ignore = "both")
+            if i === 1 
+                ignore = :start
+            elseif i === n - 2 
+                ignore = :end
+            elseif (i === 1 && i + 1 === n - 1) 
+                ignore = :both
+            end
         end
-        point_on_segment(lCoords[i], lCoords[i + 1], pCoords, ignore) && return true
+        if point_on_segment(line_points[i], line_points[i + 1], point, ignore) 
+            return true
+        end
     end
     return false
 end
 
-function point_on_segment(start, stop, coords, exclude_boundary::Symbol=:none)::Bool
-    x, y = GI.x(coords), GI.y(coords)
+function point_on_segment(start, stop, point, exclude_boundary::Symbol=:none)::Bool
+    x, y = GI.x(point), GI.y(point)
     x1, y1 = GI.x(start), GI.y(start)
     x2, y2 = GI.x(stop), GI.y(stop)
 
@@ -220,50 +231,43 @@ resides inside the polygon. The polygon can be convex or concave. The function a
 
 # Examples
 ```jldoctest
-julia> point = Point([-77, 44])
-Point([-77.0, 44.0])
+import GeoInterface as GI, GeometryOps as GO
+julia> point = (-77.0, 44.0)
+(-77.0, 44.0)
 
-julia> poly = Polygon([[ [-81, 41], [-81, 47], [-72, 47], [-72, 41], [-81, 41]]])
+julia> poly = GI.Polygon([[[-81, 41], [-81, 47], [-72, 47], [-72, 41], [-81, 41]]])
 Polygon(Array{Array{Float64,1},1}[[[-81.0, 41.0], [-81.0, 47.0], [-72.0, 47.0], [-72.0, 41.0], [-81.0, 41.0]]])
 
-julia> point_in_polygon(point, poly)
+julia> GO.point_in_polygon(point, poly)
 true
 ```
 """
-function point_in_polygon(point, polygon, ignoreBoundary::Bool=false)::Bool
+function point_in_polygon(p, polygon, ignore_boundary::Bool=false)::Bool
+    GI.trait(polygon) isa PolygonTrait || throw(ArgumentError("Not a polygon"))
+    
+    point_in_extent(p, GI.extent(polygon)) || return false
+    point_in_ring(p, GI.getexterior(polygon), ignore_boundary) || return false
 
-    pt = point.coordinates
-    poly = polygon.coordinates
-
-    in_bbox(pt, bbox(polygon)) == false && return false
-
-    geotype(polygon) === :Polygon && (poly = [poly])
-
-    inside = false
-    for i in eachindex(poly)
-        if point_in_ring(pt, poly[i][1], ignore_boundary)
-            in_hole = false
-            k = 1
-
-            while k < length(poly[i]) && !in_hole
-                inRing(pt, poly[i][k], !ignore_boundary) == true && (in_hole = true)
-                k += 1
-            end
-
-            !in_hole && (inside = true)
-        end
+    for ring in GI.gethole(polygon)
+        point_in_ring(pt, ring, !ignore_boundary) && return false
     end
-    return inside
+    return true
 end
 
 function point_in_ring(pt, ring, ignore_boundary::Bool=false)
+    GI.trait(polygon) isa Union{LineStringTrait,LinearRingTrait} || throw(ArgumentError("Not a ring"))
     inside = false
+    n = GI.npoint(ring)
+    p1 = first(GI.getpoint(ring))
+    p_end = GI.getpoint(ring, n)
 
-    if ring[1][1] == ring[length(ring) - 1][1] && ring[1][2] == ring[length(ring) - 1][1] 
-        ring = ring[1, length(ring) - 1]
+    l = if GI.x(p1) == GI.x(p_end) && GI.y(p1) == GI.y(p_end) 
+        l = n -1
+    else
+        n
     end
 
-    for i in 1:length(ring) - 1
+    for i in 1:l - 1
         j = i + 1
 
         p_i = GI.getpoint(ring, i)
@@ -273,22 +277,25 @@ function point_in_ring(pt, ring, ignore_boundary::Bool=false)
         xj = GI.x(p_j)
         yj = GI.y(p_j)
 
-        on_boundary = (pt[2] * (xi - xj) + yi * (xj - pt[1]) + yj * (pt[1] - xi) == 0) &&
-            ((xi - pt[1]) * (xj - pt[1]) <= 0) && ((yi - pt[2]) * (yj - pt[2]) <= 0)
+        on_boundary = (GI.y(pt) * (xi - xj) + yi * (xj - GI.x(pt)) + yj * (GI.x(pt) - xi) == 0) &&
+            ((xi - GI.x(pt)) * (xj - GI.x(pt)) <= 0) && ((yi - GI.y(pt)) * (yj - GI.y(pt)) <= 0)
 
         on_boundary && return !ignore_boundary
 
-        intersect = ((yi > pt[2]) !== (yj > pt[2])) && (pt[1] < (xj - xi) * (pt[2] - yi) / (yj - yi) + xi)
+        intersects = ((yi > GI.y(pt)) !== (yj > GI.y(pt))) && 
+            (GI.x(pt) < (xj - xi) * (GI.y(pt) - yi) / (yj - yi) + xi)
 
-        intersect && (inside =  !inside)
+        if intersects 
+            inside = !inside
+        end
     end
 
     return inside
 end
 
-function point_in_bbox(pt, bbox::Vector{Float64})
-    return bbox[1] <= pt[1] &&  bbox[2] <= pt[2] &&
-        bbox[3] >= pt[1] && bbox[4] >= pt[2]
+function point_in_extent(p, extent::Extents.Extent)
+    extent.X[1] <= GI.x(p) && extent.Y[1] <= GI.y(p) &&
+        extent.X[2] >= GI.x(p) && extent.Y[2] >= GI.y(p)
 end
 
 function line_in_polygon(poly, line)
@@ -311,7 +318,7 @@ function line_in_polygon(poly, line)
     return out
 end
 
-line_on_line(tline1, line2) = line_on_line(trait(line1), line1, trait(line2), line2)
+line_on_line(line1, line2) = line_on_line(trait(line1), line1, trait(line2), line2)
 function line_on_line(t1::GI.AbstractCurveTrait, line1, t2::AbstractCurveTrait, line2)
     for p in GI.getpoint(line1)
         point_on_line(p, line2) || return false
