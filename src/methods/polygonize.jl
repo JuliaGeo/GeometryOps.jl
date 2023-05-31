c6b362abf59cad31487ecf114b1e84082590d76a
Anshul Singhvi <anshulsinghvi@gmail.com>
2023-05-30 22:38:29 -0400

    Add an example and fix typo
    Changes:
    - Added a Literate/Documenter example to the file
    - Removed an extra `end` in the main `polygonize` function
    - Renamed `contours` to `get_contours`, to not conflict with other names which we might potentially import
    - Changed single-`#` inline comments to double (`##`) for Literate.jl, so it doesn't interrupt the flow.

--- src/methods/polygonize.jl
+++ src/methods/polygonize.jl
@@ -1,3 +1,55 @@
+# # Polygonizing raster data
+export polygonize
+# The methods in this file are able to convert a raster image into a set of polygons, 
+# by contour detection using a clockwise Moore neighborhood method.
+
+# The main entry point is the [`polygonize`](@ref) function.
+
+# ```@doc
+# polygonize
+# ```
+
+# ## Example
+
+# Here's a basic implementation, using the `Makie.peaks()` function.  First, let's investigate the nature of the function:
+# ```@example polygonize
+# using Makie, GeometryOps
+# n = 49
+# xs, ys = LinRange(-3, 3, n), LinRange(-3, 3, n)
+# zs = Makie.peaks(n)
+# z_max_value = maximum(abs.(extrema(zs)))
+# f, a, p = heatmap(
+#     xs, ys, zs; 
+#     axis = (; aspect = DataAspect(), title = "Exact function")
+# )
+# cb = Colorbar(f[1, 2], p; label = "Z-value")
+# f 
+# ```
+
+# Now, we can use the `polygonize` function to convert the raster data into polygons.
+
+# For this particular example, we chose a range of z-values between 0.8 and 3.2, 
+# which would provide two distinct polyogns with holes.
+
+# ```@example polygonize
+# polygons = polygonize(xs, ys, 0.8 .< zs .< 3.2)
+# ```
+# This returns a list of `GeometryBasics.Polygon`, which can be plotted immediately, 
+# or wrapped directly in a `GeometryBasics.MultiPolygon`.  Let's see how these look:
+
+# ```@example polygonize
+# f, a, p = poly(polygons; label = "Polygonized polygons", axis = (; aspect = DataAspect()))
+# ```
+
+# Finally, let's plot the Makie contour lines on top, to see how well the polygonization worked:
+# ```@example polygonize
+# contour!(a, zs; labels = true, levels = [0.8, 3.2], label = "Contour lines")
+# f
+# ```
+
+# ## Implementation
+
+# The implementation follows:
 
 """
     polygonize(A; minpoints=10)
@@ -11,14 +63,17 @@ If `xs` and `ys` are passed in they are used as the pixel center points.
 - `minpoints`: ignore polygons with less than `minpoints` points. 
 """
 polygonize(A::AbstractMatrix; kw...) = polygonize(axes(A)..., A; kw...) 
+
 function polygonize(xs, ys, A::AbstractMatrix; minpoints=10)
-    contours = Iterators.map(contours(A)) do contour
-            poly = map(contour) do xy
-                x, y = Tuple(xy)
-                Point2f(x + first(xs) - 1, y + first(ys) - 1)
-            end
+    ## This function uses a lazy map to get contours.  
+    contours = Iterators.map(get_contours(A)) do contour
+        poly = map(contour) do xy
+            x, y = Tuple(xy)
+            Point2f(x + first(xs) - 1, y + first(ys) - 1)
         end
-    end 
+    end
+    ## If we filter off the minimum points, then it's a hair more efficient
+    ## not to convert contours with length < missingpoints to polygons.
     if minpoints > 1
         contours = Iterators.filter(contours) do contour
             length(contour) > minpoints
@@ -29,12 +84,12 @@ function polygonize(xs, ys, A::AbstractMatrix; minpoints=10)
     end
 end
 
-# rotate direction clockwise
+## rotate direction clockwise
 rot_clockwise(dir) = (dir) % 8 + 1
-# rotate direction counterclockwise
+## rotate direction counterclockwise
 rot_counterclockwise(dir) = (dir + 6) % 8 + 1
 
-# move from current pixel to next in given direction
+## move from current pixel to next in given direction
 function move(pixel, image, dir, dir_delta)
     newp = pixel + dir_delta[dir]
     height, width = size(image)
@@ -46,7 +101,7 @@ function move(pixel, image, dir, dir_delta)
     return CartesianIndex(0, 0)
 end
 
-# finds direction between two given pixels
+## finds direction between two given pixels
 function from_to(from, to, dir_delta)
     delta = to - from
     return findall(x -> x == delta, dir_delta)[1]
@@ -101,18 +156,18 @@ function detect_move(image, p0, p2, nbd, border, done, dir_delta)
 end
 
 """
-   contours(A::AbstractMatrix)
+   get_contours(A::AbstractMatrix)
 
 Returns contours as vectors of `CartesianIndex`.
 """
-function contours(image::AbstractMatrix)
+function get_contours(image::AbstractMatrix)
     nbd = 1
     lnbd = 1
     image = Float64.(image)
     contour_list = Vector{typeof(CartesianIndex[])}()
     done = [false, false, false, false, false, false, false, false]
 
-    # Clockwise Moore neighborhood.
+    ## Clockwise Moore neighborhood.
     dir_delta = (CartesianIndex(-1, 0), CartesianIndex(-1, 1), CartesianIndex(0, 1), CartesianIndex(1, 1), 
                  CartesianIndex(1, 0), CartesianIndex(1, -1), CartesianIndex(0, -1), CartesianIndex(-1, -1))
 
@@ -126,7 +181,7 @@ function contours(image::AbstractMatrix)
             is_hole = (image[i, j] >= 1 && (j == width || image[i, j+1] == 0))
 
             if is_outer || is_hole
-                # 2
+                ## 2
                 border = CartesianIndex[]
                 from = CartesianIndex(i, j)
 
