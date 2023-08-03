ad8c45812dfdfe29a60f3c3a157badd2e9ca503b
Rafael Schouten <rafaelschouten@gmail.com>
2023-08-04 00:43:00 +0200

    f

--- src/methods/polygonize.jl
+++ src/methods/polygonize.jl
@@ -65,52 +65,74 @@ If `xs` and `ys` are passed in they are used as the pixel center points.
 polygonize(A::AbstractMatrix; kw...) = polygonize(axes(A)..., A; kw...) 
 function polygonize(xs::AbstractRange, ys::AbstractRange, A::AbstractMatrix{Bool}; minpoints=3)
     edges = Tuple{Tuple{Float64,Float64},Tuple{Float64,Float64}}[] 
-    xstep, ystep = step(xs), step(ys)
+    xstep, ystep = step(xs) / 2, step(ys) /2
     rings = Vector{Tuple{Float64,Float64}}[]
+    # Get all the valid edges
     for i in 2:size(A, 1)-1, j in 2:size(A, 2)-1
         if A[i, j]
-            x1, y1 = xs[i], ys[j]
-            x2, y2 = x1 + xstep, y1 + ystep
+            x, y = xs[i], ys[j]
+            x1, y1 = x - xstep, y - ystep
+            x2, y2 = x + xstep, y + ystep
             A[i, j-1] || push!(edges, ((x1, y1), (x2, y1)))
             A[i, j+1] || push!(edges, ((x1, y2), (x2, y2)))
             A[i-1, j] || push!(edges, ((x1, y1), (x1, y2)))
             A[i+1, j] || push!(edges, ((x2, y1), (x2, y2)))
         end
-        @show edges
     end
+    # Then join them into polygons
     ring = Tuple{Float64,Float64}[]
-    dir = 1
     while length(edges) > 0
         edge = pop!(edges)
         firstpoint = first(edge)
         nextpoint = last(edge)
         ring = [firstpoint, nextpoint]
         push!(rings, ring)
-        while length(edges) > 0
+        while true
+            # Find an edge that matches the next point
             i = findfirst(e -> nextpoint in e, edges)
-            isnothing(i) && break
             edge = popat!(edges, i)
-            newpoint = otherpoint(edge, nextpoint)
-            if newpoint == firstpoint
-                push!(ring, newpoint)
+            nextpoint = otherpoint(edge, nextpoint)
+            # Close if we get to the start
+            if nextpoint == firstpoint
+                push!(ring, nextpoint)
                 break
-            # elseif newpoint in ring
-            #     i = findfirst(==(newpoint), ring)
-            #     splitring = ring[i:lastindex(ring)]
-            #     deleteat!(ring, i:lastindex(ring))
-            #     push!(splitring, newpoint)
-            #     push!(rings, splitring)
-            #     nextpoint = last(ring)
-            #     continue
+            # Split if we touch the ring
+            elseif nextpoint in ring
+                i = findfirst(==(nextpoint), ring)
+                splitring = ring[i:lastindex(ring)]
+                deleteat!(ring, i:lastindex(ring))
+                push!(ring, nextpoint)
+                push!(splitring, nextpoint)
+                push!(rings, splitring)
+                continue
+            # Otherwise keep adding points
             else
-                push!(ring, newpoint)
-                nextpoint = newpoint
+                push!(ring, nextpoint)
             end
         end
     end
-    map(rings) do ring
-        GB.Polygon(map(GB.Point, ring))
+    i = 1
+    blacklist = Set{Int}()
+    polylist = Set{Int}()
+    polygons = map(x -> [GI.LinearRing(x)], rings)
+    while i <= length(rings)
+        push!(blacklist, i)
+        ring_i = GI.LinearRing(rings[i]) 
+        for j in 1:length(rings)
+            j in blacklist && continue
+            ring_j = GI.LinearRing(rings[j]) 
+            if polygon_in_polygon(ring_j, ring_i)
+                push!(blacklist, j)
+                push!(polygons[i], ring_j)
+                push!(polylist, i)
+            elseif polygon_in_polygon(ring_i, rings_j)
+                push!(blacklist, j)
+                push!(polygons[j], ring_i)
+                push!(polylist, j)
+            end
+        end
     end
+    return GI.Polygon.(polygons[sort!(collect(polylist))])
 end
 
 otherpoint(edge, point) = first(edge) == point ? last(edge) : first(edge)
