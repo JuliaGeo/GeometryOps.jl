201c254492c95b155a024482a0ec6a98d581039f
Rafael Schouten <rafaelschouten@gmail.com>
2023-08-04 22:26:02 +0200

    some fixes

--- src/methods/polygonize.jl
+++ src/methods/polygonize.jl
@@ -1,6 +1,6 @@
 # # Polygonizing raster data
 export polygonize
-# The methods in this file are able to convert a raster image into a set of polygons, 
+# The methods in this file are able to convert a raster image into a set of polygons,
 # by contour detection using a clockwise Moore neighborhood method.
 
 # The main entry point is the [`polygonize`](@ref) function.
@@ -19,22 +19,22 @@ export polygonize
 # zs = Makie.peaks(n)
 # z_max_value = maximum(abs.(extrema(zs)))
 # f, a, p = heatmap(
-#     xs, ys, zs; 
+#     xs, ys, zs;
 #     axis = (; aspect = DataAspect(), title = "Exact function")
 # )
 # cb = Colorbar(f[1, 2], p; label = "Z-value")
-# f 
+# f
 # ```
 
 # Now, we can use the `polygonize` function to convert the raster data into polygons.
 
-# For this particular example, we chose a range of z-values between 0.8 and 3.2, 
+# For this particular example, we chose a range of z-values between 0.8 and 3.2,
 # which would provide two distinct polyogns with holes.
 
 # ```@example polygonize
 # polygons = polygonize(xs, ys, 0.8 .< zs .< 3.2)
 # ```
-# This returns a list of `GeometryBasics.Polygon`, which can be plotted immediately, 
+# This returns a list of `GeometryBasics.Polygon`, which can be plotted immediately,
 # or wrapped directly in a `GeometryBasics.MultiPolygon`.  Let's see how these look:
 
 # ```@example polygonize
@@ -60,52 +60,62 @@ Convert matrix `A` to polygons.
 If `xs` and `ys` are passed in they are used as the pixel center points.
 
 # Keywords
-- `minpoints`: ignore polygons with less than `minpoints` points. 
+- `minpoints`: ignore polygons with less than `minpoints` points.
 """
-polygonize(A::AbstractMatrix; kw...) = polygonize(axes(A)..., A; kw...) 
-function polygonize(xs::AbstractRange, ys::AbstractRange, A::AbstractMatrix{Bool}; minpoints=3)
-    # Define buffers for edges and rings
-    edges = Tuple{Tuple{Float64,Float64},Tuple{Float64,Float64}}[] 
-    rings = Vector{Tuple{Float64,Float64}}[]
+polygonize(A::AbstractMatrix; kw...) = polygonize(axes(A)..., A; kw...)
+function polygonize(xs::AbstractRange, ys::AbstractRange, A::AbstractMatrix{Bool})
+    # Make ranges of pixel bounds
+    xbounds = first(xs) - step(xs) / 2 : step(xs) : last(xs) + step(xs) / 2 
+    ybounds = first(ys) - step(ys) / 2 : step(ys) : last(ys) + step(ys) / 2 
+    return _polygonize(xbounds, ybounds, A)
+end
+function polygonize(xs::AbstractVector, ys::AbstractVector, A::AbstractMatrix{Bool})
+    _polygonize(xs, ys, A)
+end
 
-    # First get all the valid edges between 
-    # filled an empty pixels
-    halfxstep, halfystep = step(xs) / 2, step(ys) /2
+function _polygonize(xs::AbstractVector{T}, ys::AbstractVector{T}, A::AbstractMatrix{Bool}) where T
+    # Define buffers for edges and rings
+    edges = Tuple{Tuple{T,T},Tuple{T,T}}[]
+    rings = Vector{Tuple{T,T}}[]
+
+    # First get all the valid edges between filled and empty pixels
     si, sj = map(last, axes(A))
     for i in axes(A, 1), j in axes(A, 2)
         if A[i, j] # This is a pixel inside a polygon
-            # xs and ys hold pixel centers
-            x, y = xs[i], ys[j]
-            # So we need to offset the edges by half
-            x1, y1 = x - halfxstep, y - halfystep
-            x2, y2 = x + halfxstep, y + halfystep
-            # Then we check the Von Neumann neighborhood to
+            # xs and ys hold pixel bounds
+            x1, x2 = xs[i], xs[i + 1] 
+            y1, y2 = ys[j], ys[j + 1] 
+            # We check the Von Neumann neighborhood to
             # decide what edges are needed, if any.
-            j >= 1 && !A[i, j-1] && push!(edges, ((x1, y1), (x2, y1)))
-            i >= 1 && !A[i-1, j] && push!(edges, ((x1, y2), (x1, y1)))
-            j <= sj && !A[i, j+1] && push!(edges, ((x2, y2), (x1, y2)))
-            i <= si && !A[i+1, j] && push!(edges, ((x2, y1), (x2, y2)))
+            (j == 1 || !A[i, j-1]) && push!(edges, ((x1, y1), (x2, y1)))
+            (i == 1 || !A[i-1, j]) && push!(edges, ((x1, y2), (x1, y1)))
+            (j == sj || !A[i, j+1]) && push!(edges, ((x2, y2), (x1, y2)))
+            (i == si || !A[i+1, j]) && push!(edges, ((x2, y1), (x2, y2)))
         end
     end
 
     # Then we join the edges into rings
+    # Sorting now lets us use `searchsortedlast` for speed later
     sort!(edges)
     while length(edges) > 0
+        # Take the last edge from the array
         edge = pop!(edges)
-        firstpoint::Tuple{Float64,Float64} = first(edge)
-        nextpoint::Tuple{Float64,Float64} = last(edge)
+        firstpoint::Tuple{T,T} = first(edge)
+        nextpoint::Tuple{T,T} = last(edge)
         ring = [firstpoint, nextpoint]
         push!(rings, ring)
-        while true
+        while length(edges) > 0
             # Find an edge that matches the next point
             i = searchsortedlast(edges, (nextpoint, nextpoint); by=first)
+            # TODO: Here we actually need to check which edge maintains
+            # the winding direction
             edge = popat!(edges, i)
             nextpoint = last(edge)
             # Close the ring if we get to the start
             if nextpoint == firstpoint
                 push!(ring, nextpoint)
                 break
-                # Split off another ring if we touch the current ring anywhere else
+            # Split off another ring if we touch the current ring anywhere else
             elseif nextpoint in ring
                 i = findfirst(==(nextpoint), ring)
                 splitring = ring[i:lastindex(ring)]
@@ -121,21 +131,26 @@ function polygonize(xs::AbstractRange, ys::AbstractRange, A::AbstractMatrix{Bool
         end
     end
 
-    # Define wrapped LinearRings, with embedded extents 
+    # Define wrapped LinearRings, with embedded extents
     # so we only calculate them once
     linearrings = map(rings) do ring
         extent = GI.extent(GI.LinearRing(ring))
         GI.LinearRing(ring; extent)
     end
-    # Separate exteriors from holes by winding direction
-    clockwise = isclockwise.(linearrings)
-    polygons = map(x -> [x], linearrings[.!clockwise])
-    holes = map(x -> x, linearrings[clockwise])
 
-    # Then we add holes to the polygons
+    # Separate exteriors from holes by winding direction
+    direction = last(xs) - first(xs) * last(ys) - first(ys)
+    exterior_inds = if direction > 0 
+        .!isclockwise.(linearrings)
+    else
+        isclockwise.(linearrings)
+    end
+    polygons = map(x -> [x], linearrings[exterior_inds])
+    holes = map(x -> x, linearrings[.!exterior_inds])
+
+    # Then we add the holes to the polygons they are inside of
     blacklist = Set{Int}()
-    polylist = Set{Int}()
-    foreach(polygons) do rings
+    for rings in polygons
         for i in eachindex(holes)
             i in blacklist && continue
             exterior = rings[1]
