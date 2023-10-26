9b645749db0e41c7263d31da58677fe216fa9b3a
Skylar Gering <sgering@g.hmc.edu>
2023-10-25 17:10:57 -0700

    Remove trial file

--- src/try.jl
+++
@@ -1,7 +0,0 @@
-import GeometryOps as GO
-import GeoInterface as GI
-import LibGEOS as LG
-
-r1 = LG.LinearRing([[0.0, 0.0], [5.0, 5.0], [10.0, 0.0], [5.0, -5.0], [0.0, 0.0]])
-r2 = LG.LinearRing([[3.0, 0.0], [8.0, 5.0], [13.0, 0.0], [8.0, -5.0], [3.0, 0.0]])
-GO.equals(r1, r1)
