6933aa61530f0b512f4349a53e0934745c3fef08
LanaLubecke <llubecke@caltech.edu>
2023-11-06 11:14:36 -0800

    Add more comments

--- src/methods/intersects.jl
+++ src/methods/intersects.jl
@@ -210,60 +210,100 @@ intersection(
 
 Calculates the intersection between two line segments. Return nothing if
 there isn't one.
+## Example
+
+```jldoctest
+import GeoInterface as GI, GeometryOps as GO
+
+p1 = GI.Polygon([[(0.0, 0.0), (5.0, 5.0), (10.0, 0.0), (5.0, -5.0), (0.0, 0.0)]])
+p2 = GI.Polygon([[(3.0, 0.0), (8.0, 5.0), (13.0, 0.0), (8.0, -5.0), (3.0, 0.0)]])
+GO.intersection(p1, p2)
+
+# output
+1-element Vector{Vector{Tuple{Float64, Float64}}}:
+[(6.5, 3.5), (10.0, 0.0), (6.5, -3.5), (3.0, 0.0), (6.5, 3.5)]
+```
 """
-
-
-# example polygon from Greiner paper
-# p3 = GI.Polygon([[[0.0, 0.0], [0.0, 4.0], [7.0, 4.0], [7.0, 0.0], [0.0, 0.0]]])
-# p4 = GI.Polygon([[[1.0, -3.0], [1.0, 1.0], [3.5, -1.5], [6.0, 1.0], [6.0, -3.0], [1.0, -3.0]]])
-# p5 = GI.Polygon([[(1.0, -3.0), (1.0, 1.0), (3.5, -1.5), (6.0, 1.0), (6.0, -3.0), (1.0, -3.0)]])
-
-
 function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
-    # check if they intersect
-    if !intersects(poly_a, poly_b)
-        return [[]]
-    end
-    # makes a list for each polygon
+        
+    # Make a list for nodes of each polygon. Note the definition of PolyNode
     a_list = Array{PolyNode, 1}(undef, _nedge(poly_a))
     b_list = Array{PolyNode, 1}(undef, _nedge(poly_b))
 
-    # fix guess of 10
-    intr_list = Array{Tuple{Real, Real}, 1}(undef, 10)
-    a_idx_list = Array{Int, 1}(undef, 10)
-    b_idx_list = Array{Int, 1}(undef, 10)
-    alpha_a_list = Array{Real, 1}(undef, 10)
-    alpha_b_list = Array{Real, 1}(undef, 10)
+    # Initialize arrays to keep track of the important information 
+    # associated with the intersection points of poly_a and poly_b.
+    # I initialize these arrays assumed there will be a maximum of 
+    # 30 intersection points and then I truncate the arrays later.
+    k = 30
+    # intr_list stores the cartesian coordintes of the intersection point
+    intr_list = Array{Tuple{Real, Real}, 1}(undef, k)
+    # At index i, a_idx_list stores the index of the ith intersection point in a_list
+    a_idx_list = Array{Int, 1}(undef, k)
+    # At index i, b_idx_list stores the edge number of poly_b that the ith intersection
+    # point lies on.
+    b_idx_list = Array{Int, 1}(undef, k)
+    # Alpha values are used to determine the order in which to place
+    # intersection points in a polygon list, which is especially
+    # useful when many intersection points lie on the same edge of a polygon.
+    alpha_a_list = Array{Real, 1}(undef, k)
+    alpha_b_list = Array{Real, 1}(undef, k)
 
+    # These lists store the cartesian coordinates of poly_a and poly_b
     edges_a = to_edges(poly_a)
     edges_b = to_edges(poly_b)
-    # iterates through edges of each polygon
+
+    # # if poly_a contained in poly_b, doesn't work yet
+    # if polygon_in_polygon(poly_a, poly_b)
+    #     return_polys = Vector{Vector{Tuple{Float64, Float64}}}(undef, 0)
+    #     for edge in edges_a
+    #         push!(return_polys, edge[1])
+    #     end
+    #     return return_polys
+    # end
+
+    # # if poly_b contained in poly_a, doesn't work yet
+    # if polygon_in_polygon(poly_b, poly_a)
+    #     return_polys = Vector{Vector{Tuple{Float64, Float64}}}(undef, 0)
+    #     for edge in edges_b
+    #         push!(return_polys, edge[1])
+    #     end
+    #     return return_polys
+    # end
+
+    # Find intersection points and adds them to a_list
+    # "counter" is used to index all inter-related lists
     counter = 1
+    # "acount" is used to index a_list
     acount = 1
     for ii in eachindex(edges_a)
-        # add the first point of the edge to the list of points in a
+        # Add the first point of the edge to the list of points in a_list
         if acount <= length(a_list)
             a_list[acount] = PolyNode(ii, false, 0, false, 0, false)
         else
             push!(a_list, PolyNode(ii, false, 0, false, 0, false))
         end
         acount = acount + 1
+
+        # Keep track of current position in inter-related lists
+        # before finding new intersection points on our new edge
         prev_counter = counter
 
         for jj in eachindex(edges_b)
 
-            # add the first point of the edge to the list of points in b
+            # Add the first point of the edge to b_list
             if ii == 1 
                 b_list[jj] = PolyNode(jj, false, 0, false, 0, false)
             end
 
-            # checks if edges intersect
-            # there is got to be a better way to check if the edges intersect
+            # Check if edge jj of poly_b intersects with edge ii of poly_a
             if _line_intersects([edges_a[ii]], [edges_b[jj]]);
                 
                 int_pt, alphas = _intersection_point(edges_a[ii], edges_b[jj])
+                # Store the cartesion coordinates of intersection point
                 intr_list[counter] = int_pt
+                # Store which edge of poly_b the intersection point lies on
                 b_idx_list[counter] = jj
+                # Store the alpha values
                 alpha_a_list[counter] = alphas[1]
                 alpha_b_list[counter] = alphas[2]
 
@@ -273,15 +313,20 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
             end
         end
 
-         # add the intersection point to a list if we found any
+        # After iterating through all edges of poly_b for edge ii of poly_a,
+        # add the intersection points to a_list in CORRECT ORDER if we found any
         if prev_counter < counter
+            # Order intersection points based on alpha values
             new_order = sortperm(alpha_a_list[prev_counter:counter-1])
             pts_to_add = Array{PolyNode, 1}(undef, counter - prev_counter)
             for kk in eachindex(new_order)
+                # Create PolyNodes of the new intersection points in the correct order
+                # and store the correct index in a_idx_list
                 pts_to_add[new_order[kk]] = PolyNode(prev_counter+kk-1, true, 0, false, alpha_a_list[prev_counter+kk-1], false)
                 a_idx_list[prev_counter+kk-1] = acount + new_order[kk] - 1;
             end
 
+            # Add the PolyNodes to a_list and update acount
             splice!(a_list, acount:(acount-1), pts_to_add)
             acount = acount + counter - prev_counter
             
@@ -289,16 +334,18 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
         
     end
 
+    # Truncate the inter_related lists
     intr_list = intr_list[1:counter-1] 
     a_idx_list = a_idx_list[1:counter-1]
     b_idx_list = b_idx_list[1:counter-1]
     alpha_a_list = alpha_a_list[1:counter-1]
     alpha_b_list = alpha_b_list[1:counter-1]
 
-    # now iterate through the b_list and add in intersection points
+    # Iterate through the b_list and add in intersection points
+    # Occasionally I need to skip the new points I added to the array
     skip = false
     num_skips = 0
-    b_neighbors = Array{Int, 1}(undef, 10)
+    b_neighbors = Array{Int, 1}(undef, k)
     for ii in 1:(length(b_list)+length(intr_list))
         # TODO: this skipping scheme could be made nicer
         if skip
@@ -308,11 +355,11 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
             end
             continue
         end
-        # find the idx in the intr_list (same as b_idx_list) where the intr point is
-        # find all is inefficient though, so it might be better to make dictionary of indices
+        # Find the index in the intr_list (same as b_idx_list) where the inter point is
+        # TODO:find all is inefficient though, so it might be better to make dictionary of indices
         i = findall(x->x==b_list[ii].idx, b_idx_list)
         if !isempty(i)     
-            # sort perm puts intersection pts in order of alpha value
+            # Order intersection points based on alpha values
             new_order = sortperm(alpha_b_list[i])
             pts_to_add = Array{PolyNode, 1}(undef, length(i))
             for m in eachindex(i)
@@ -327,14 +374,14 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
     end
 
     b_neighbors = b_neighbors[1:counter-1]
-    # finally, iterate through a_list and update the neighbor indices
+    # Iterate through a_list and update the neighbor indices
     for ii in eachindex(a_list)
         if a_list[ii].inter
             a_list[ii].neighbor = b_neighbors[a_list[ii].idx]
         end
     end
 
-    # put in ent exit flags for polygon a
+    # Put in ent exit flags for poly_a
     status = false
     for ii in eachindex(a_list)
         if ii == 1
@@ -348,7 +395,7 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
         end
     end
 
-    # put in ent exit flags for polygon b
+    # Put in ent exit flags for poly_b
     status = false
     for ii in eachindex(b_list)
         if ii == 1
@@ -362,30 +409,33 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
         end
     end
 
-    # array for return polygons
-    return_polys = Array{Array{Tuple{Float64, Float64}, 1}, 1}(undef, 0)
-    counter = 0
+    # Pre-allocate array for return polygons
+    return_polys = Vector{Vector{Tuple{Float64, Float64}}}(undef, 0)
+    # TODO: Make sure can remove this counter
+    # counter = 0
     
-    # keeping track of processed intersection points
+    # Keep track of number of processed intersection points
     processed_pts = 0
     tracker = copy(a_idx_list)
     
     while processed_pts < length(intr_list)
-        # way to toggle between the lists
+        # Create variables "list_edges" and "list" so that we can toggle between
+        # a_list and b_list
         list_edges = edges_a
         list = a_list
 
-        # finding index of first unprocessed intersecting point in subject polygon
+        # Find index of first unprocessed intersecting point in subject polygon
         starting_pt = minimum(tracker)
         idx = starting_pt
 
-        # Getting current first unprocessed intersection point
+        # Get current first unprocessed intersection point PolyNode
         current = a_list[idx]
-        # array to store the intersection polygon points
-        pt_list = Array{Tuple{Float64, Float64}, 1}(undef, 0)
+        # Initialize array to store the intersection polygon cartesian points
+        pt_list = Vector{Tuple{Float64, Float64}}(undef, 0)
+        # Add the first point to the array
         push!(pt_list, (intr_list[current.idx][1], intr_list[current.idx][2]))
         
-        # marking frist intersection point as processes
+        # Mark first intersection point as processed
         processed_pts = processed_pts + 1
         tracker[current.idx] = typemax(Int)
 
@@ -399,45 +449,47 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
                     status2 = current.ent_exit
                 end
 
+                # Depending on status of first intersection point, either
+                # traverse polygon forwards or backwards
                 if status2
                     idx = idx + 1
                 else
                     idx = idx -1
                 end
 
-                # wrapping around the point list
+                # Wrap around the point list
                 if idx > length(list)
                     idx = mod(idx, length(list))
                 elseif idx == 0
                     idx = length(list)
                 end
 
-                # getting current node
+                # Get current node
                 current = list[idx]
 
-                # adding the current node to the pt_list is a little complicated
+                # Add current node to the pt_list
                 if current.inter
-                    # add coords from inter list
+                    # Add cartesian coordinates from inter_list
                     push!(pt_list, (intr_list[current.idx][1], intr_list[current.idx][2]))
                     
-                    # keeping track of processed intersection points
+                    # Keep track of processed intersection points
                     if (current != a_list[starting_pt] && current != b_list[a_list[starting_pt].neighbor])
                         processed_pts = processed_pts + 1
                         tracker[current.idx] = typemax(Int)
                     end
                     
                 else
-                    # add coords from "list", which should point to either a or b list
+                    # Add cartesian coordinates from "list", which should point to either a_list or b_list
                     push!(pt_list, (list_edges[current.idx][1][1], list_edges[current.idx][1][2]))
                 end
 
                 current_node_not_intersection = !current.inter
             end
                
-            # break once get back to starting point
+            # Break once get back to starting point
             current_node_not_starting = (current != a_list[starting_pt] && current != b_list[a_list[starting_pt].neighbor])
 
-            # switch to neighbor list
+            # Switch to neighbor list
             if list == a_list
                 list = b_list
                 list_edges = edges_b
@@ -450,7 +502,8 @@ function intersection(::GI.PolygonTrait, poly_a, ::GI.PolygonTrait, poly_b)
         end
 
         push!(return_polys, pt_list)
-        counter = counter + 1
+        # TODO: Make sure can remove this counter
+        # counter = counter + 1
     end
     return return_polys
 end
