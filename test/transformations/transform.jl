135be02ad4153f258f40f07d786520efde43644a
Rafael Schouten <rafaelschouten@gmail.com>
2024-01-02 13:44:20 +0100

    add transform method for coordinate transformations (#35)
    * add transform method for coordinate transformations
    
    * clean up tests
    
    * actually run the test
    
    * dont flip!
    
    * no flip
    
    * fix the doc

---
+++ test/transformations/transform.jl
@@ -0,0 +1,14 @@
+using Test
+ 
+import GeoInterface as GI
+import GeometryOps as GO
+using GeometryOps.Proj.CoordinateTransformations
+
+@testset "transform" begin
+    geom = GI.Polygon([GI.LinearRing([(1, 2), (3, 4), (5, 6), (1, 2)]), 
+                       GI.LinearRing([(3, 4), (5, 6), (6, 7), (3, 4)])])
+    translated = GI.Polygon([GI.LinearRing([[4.5, 3.5], [6.5, 5.5], [8.5, 7.5], [4.5, 3.5]]), 
+                             GI.LinearRing([[6.5, 5.5], [8.5, 7.5], [9.5, 8.5], [6.5, 5.5]])])
+    f = CoordinateTransformations.Translation(3.5, 1.5)
+    @test GO.transform(f, geom) == translated
+end
