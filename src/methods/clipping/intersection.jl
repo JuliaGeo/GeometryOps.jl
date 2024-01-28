d3b9e9f1653c0d23fdfd0c41d1be25820a6628c8
LanaLubecke <llubecke@caltech.edu>
2024-01-28 15:58:52 -0800

    fixes doc string for intersection

--- src/methods/clipping/intersection.jl
+++ src/methods/clipping/intersection.jl
@@ -3,7 +3,7 @@ export intersection, intersection_points
 
 
 """
-    intersection(geom_a, geom_b, ::Type{T}; target::Type{Target})
+    intersection(geom_a, geom_b, [T::Type]; target::Type)
 
 Return the intersection between two geometries as a list of geometries. Return an empty list
 if none are found. The type of the list will be constrained as much as possible given the
