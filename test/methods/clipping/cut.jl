6d16fb96183639317401f189e042cd46f031c3e7
Skylar Gering <sgering@g.hmc.edu>
2024-02-02 14:24:25 -0600

    Add cut tests that throw warnings

--- test/methods/clipping/cut.jl
+++ test/methods/clipping/cut.jl
@@ -1,6 +1,8 @@
 l1 = GI.Line([(5.0, -5.0), (5.0, 15.0)])
 l2 = GI.Line([(-1.0, 8.0), (2.0, 11.0)])
 l3 = GI.Line([(-1.0, 6.0), (11.0, 6.0)])
+l4 = GI.Line([(-10.0, -10.0), (-1.0, -1.0)])
+l5 = GI.Line([(-10.0, 5.0), (5.0, 5.0)])
 r1 = GI.LinearRing([(0.0, 0.0), (10.0, 0.0), (10.0, 10.0), (0.0, 10.0), (0.0, 0.0)])
 r2 = GI.LinearRing([(2.0, 2.0), (8.0, 2.0), (8.0, 4.0), (2.0, 4.0), (2.0, 2.0)])
 p1 = GI.Polygon([r1])
@@ -26,7 +28,6 @@ p3 = GI.Polygon([[(0.0, 0.0), (0.0, 5.0), (2.5, 7.5), (5.0, 5.0), (7.5, 7.5), (1
             GI.Polygon([[(0.0, 9.0), (0.0, 10.0), (1.0, 10.0), (0.0, 9.0)]]),
         ],
     ))
-
 
     # Cut convex polygon with hole
     cut_polys = GO.cut(p2, l1)
@@ -56,4 +57,11 @@ p3 = GI.Polygon([[(0.0, 0.0), (0.0, 5.0), (2.5, 7.5), (5.0, 5.0), (7.5, 7.5), (1
             GI.Polygon([[(6.0, 6.0), (7.5, 7.5), (9.0, 6.0), (6.0, 6.0)]]),
         ],
     ))
+
+    # Line doesn't cut through polygon
+    . = "Line doesn't fully cut through polygon. Returning original polygon."
+    cut_polys = (@test_logs (:warn, warn_str) GO.cut(p1, l4))
+    @test all(GO.equals.(cut_polys, [p1]))
+    cut_polys = (@test_logs (:warn, warn_str) GO.cut(p1, l5))
+    @test all(GO.equals.(cut_polys, [p1]))
 end
