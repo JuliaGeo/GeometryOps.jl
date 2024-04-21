47169657811fc36c79d56fd822eb7e210a539dfe
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-21 19:02:13 -0400

    `geometry_providers.jl`: Remove redundancy, add comments

--- benchmarks/geometry_providers.jl
+++ benchmarks/geometry_providers.jl
@@ -8,57 +8,51 @@ c. performance issues in the packages' implementations of GeoInterface
 =#
 
 # First, we import the providers:
-using ArchGDAL, LibGEOS, Shapefile, GeoJSON, WellKnownGeometry, GeometryBasics, GeoInterface
-
-import GeometryOps as GO, GeoInterface as GI
-
-using BenchmarkTools, Chairmarks, CairoMakie, MakieThemes, DataFrames, Proj
-
+using ArchGDAL, LibGEOS, Shapefile, GeoJSON, WellKnownGeometry, GeometryBasics, GeoInterface, GeoFormatTypes
 PROVIDERS = (ArchGDAL, LibGEOS, GeometryBasics, GI.Wrappers)
+# Now, we import GeoInterface and GeometryOps,
+import GeometryOps as GO, GeoInterface as GI
+# Finally, we import some utility benchmarking, plotting and data munging packages!
+using BenchmarkTools, Chairmarks, CairoMakie, MakieThemes, DataFrames, Proj
+using CoordinateTransformations, Rotations
 
 
+# Polylabel.jl is a package that finds the "pole of inaccessibility" of a polygon,
+# i.e., the point within it that is furthest away from its boundaries.  
+
+# It depends on GeometryOps, but in this instance, we'll grab some of its test geometries
+# to use.
 import Polylabel
 
-water1 = ArchGDAL.fromWKT([readchomp(joinpath(dirname(dirname(pathof(Polylabel))), "test", "data", "water1.wkt")) |> String])
-water2 = ArchGDAL.fromWKT([readchomp(joinpath(dirname(dirname(pathof(Polylabel))), "test", "data", "water2.wkt")) |> String])
+# TODO: the reason we change to LibGEOS intermediately here is so that the 
+# linear rings of the WKG polygons are interpreted correctly.  Unfortunately 
+# that doesn't work when read, which there's an issue up for.
+water1 = GeoFormatTypes.WellKnownText(GeoFormatTypes.Geom(), readchomp(joinpath(dirname(dirname(pathof(Polylabel))), "test", "data", "water1.wkt")) |> String) |> x -> GI.convert(LibGEOS, x) |> GO.tuples
+water2 = GeoFormatTypes.WellKnownText(GeoFormatTypes.Geom(), readchomp(joinpath(dirname(dirname(pathof(Polylabel))), "test", "data", "water2.wkt")) |> String) |> x -> GI.convert(LibGEOS, x) |> GO.tuples
+# To fix these polygons is a complicated task, and even then LibGEOS gets it wrong:
+# water1 |> x -> LibGEOS.makeValid(GI.convert(LibGEOS, x)) |> GI.getgeom |> collect |> x -> filter(y -> GI.trait(y) isa Union{GI.PolygonTrait, GI.MultiPolygonTrait}, x) |> first |> GO.tuples # hide
 
-water1_1 = GO.fix(water1, corrections = [GO.GEOSCorrection()])
-water2 = GO.fix(water2, corrections = [GO.GEOSCorrection()])
-
-using CoordinateTransformations, Rotations
-w1rg = GO.transform(Translation(GO.centroid(water1)) ∘ LinearMap(Makie.rotmatrix2d(π/2)) ∘ Translation((-).(GO.centroid(water1))), water1)
-water1r = GI.convert(ArchGDAL, w1rg)
-using GeometryOps
-w1g = GI.Polygon(GI.LinearRing.(getproperty.(GO.tuples(water1).geom, :geom)))
-w1rg = GI.Polygon(GI.LinearRing.(getproperty.(GO.tuples(w1rg).geom, :geom)))
-w2g = GI.Polygon(GI.LinearRing.(getproperty.(GO.tuples(water2).geom, :geom)))
-
-
-self_intersection = Point2f[(0,0), (0,1), (1, 0), (1, 1), (0, 0)]
-
-sip = GI.Polygon([GI.LinearRing(self_intersection)])
-
-sip = GO.fix(sip, corrections = [GO.GEOSCorrection()])
-
-LibGEOS.makeValid(GI.convert(LibGEOS, sip))
-
-f, a, p = poly(w1g)
-poly!(w1rg)
+f, a, p = poly(water1; axis = (; title = "water1")); poly(f[1, 2], water2; axis = (; title = "water2")); f
+# Now, we rotate the `water1` polygon about its centroid, so we can use it to 
+# test the time it takes to intersect complex polygons:
+water1r = GO.transform(
+    Translation(GO.centroid(water1)) ∘ LinearMap(Makie.rotmatrix2d(π/2)) ∘ Translation((-).(GO.centroid(water1))), 
+    water1
+)
+f, a, p = poly(water1; label = "Original")
+poly!(water1r; label = "Rotated")
+axislegend(a)
 f
-
-w1l, w1rl = GI.convert.((LibGEOS,), (water1, water1r))
-w1l = LibGEOS.makeValid(w1l)
-w1rl = LibGEOS.makeValid(w1rl)
-
-@b GO.union($w1g, $w1rg; target = GI.PolygonTrait()) seconds=3
-@b LibGEOS.union($w1l, $w1rl) seconds=3
-@b ArchGDAL.union($water1, $water1r) seconds=3
+# WARNING: does not work
+@b GO.union($(water1), $(water1r); target = GI.PolygonTrait()) seconds=3
+@b LibGEOS.union($(GI.convert(LibGEOS, water1)), $(GI.convert(LibGEOS, water1r))) seconds=3
+@b ArchGDAL.union($(GI.convert(ArchGDAL, water1)), $(GI.convert(ArchGDAL, water1r))) seconds=3
 
 poly(GO.union(w1g, w1rg; target = GI.PolygonTrait()))
 
 GI.getgeom(water1, 3) |> GI.trait
 
-GO.tuples(water1)
+# We can benchmark each provider and see if any of them have glaring issues.
 
 water1_centroid_suite = BenchmarkGroup()
 
@@ -68,23 +62,24 @@ for provider in PROVIDERS
     water1_centroid_suite[string(provider)] = @be GO.centroid($geom) seconds=3
 end
 
+
+# ## Tables.jl performance in `apply`
+#=
+This code checks how Tables.jl performs when using `apply`.
+We use two sources for this: `Shapefile.jl` and `DataFrames.jl`.
+More will be coming in the future!
+=#
 shp_file = "/Users/anshul/Downloads/ne_10m_admin_0_countries (1)/ne_10m_admin_0_countries.shp"
 table = Shapefile.Table(shp_file)
-
-GO.apply(identity, GI.PointTrait(), table) |> typeof
-
-using Proj
-all_admin1 = GeoJSON.read(read(download("https://rawcdn.githack.com/nvkelso/natural-earth-vector/v5.1.2/geojson/ne_10m_admin_1_states_provinces.geojson"), String))
+go_df = DataFrame(table)
+go_df.geometry = GO.tuples(go_df.geometry);
 
 table_suite = BenchmarkGroup()
 
 
-go_df = DataFrame(table)
-go_df.geometry = GO.tuples(go_df.geometry);
-
 ll2moll = Proj.Transformation("+proj=longlat +datum=WGS84", "+proj=moll")
 
-
+# First, we try reprojecting the geometries using Proj,
 reproject_suite = table_suite["reproject"] = BenchmarkGroup(["title:Reproject", "subtitle:All country borders from Natural Earth, 1:10m res."])
 
 reproject_suite["Shapefile.Table"] = @be GO.reproject($table, $ll2moll) seconds=3
@@ -93,6 +88,8 @@ reproject_suite["DataFrame (GO)"] = @be GO.reproject($(go_df), $ll2moll) seconds
 reproject_suite["Shapefile geoms"] = @be GO.reproject($(table.geometry), $ll2moll) seconds=3
 reproject_suite["GeometryOps geoms"] = @be GO.reproject($(GO.tuples(table.geometry)), $ll2moll) seconds=3
 
+# then transforming, just to see the difference in runtime
+# between calling out to C vs pure Julia,
 function _scaleby5(x)
     return x .* 5
 end
@@ -104,6 +101,8 @@ transform_suite["DataFrame (GO)"] = @be GO.transform($_scaleby5, $(go_df)) secon
 transform_suite["Shapefile geoms"] = @be GO.transform($_scaleby5, $(table.geometry)) seconds=3
 transform_suite["GeometryOps geoms"] = @be GO.transform($_scaleby5, $(GO.tuples(table.geometry))) seconds=3
 
+# and finally, calling `applyreduce` to find the area of each
+# polygon.
 area_suite = table_suite["area"] = BenchmarkGroup(["title:Area", "subtitle:All country borders from Natural Earth, 1:10m res."])
 
 area_suite["Shapefile.Table"] = @be GO.area($(table)) seconds=3
@@ -116,6 +115,8 @@ ts = getproperty.(area_suite["Shapefile.Table"].samples, :time)
 boxplot(ones(length(ts)), ts)
 violin(ones(length(ts)), ts; npoints = 3500, axis = (; yscale = log10,))
 
+
+# ## Plotting
 function Makie.convert_arguments(::Makie.PointBased, xs, bs::AbstractVector{<: Chairmarks.Benchmark})
     ts = getproperty.(Statistics.mean.(bs), :time)
     return (xs, ts)
