d5fedc6f161bdc652d7de39a18d141c30a3b3c5e
Rafael Schouten <rafaelschouten@gmail.com>
2024-05-15 17:43:19 +0200

    more tests

--- test/methods/polygonize.jl
+++ test/methods/polygonize.jl
@@ -3,26 +3,37 @@ using GeometryOps, GeoInterface, Test
 # Missing holes throw a warning, so testing there are
 # no warnings in a range of randomisation is one way to test 
 # that things are working, without testing explicit return values
-for i in (100, 300), j in (100, 300), x in 0.1:0.2:0.9
-    i = j = 100
-    A = rand(i, j)
-    @test_nowarn multipolygon = polygonize(>(x), A);
-    @test multipolygon isa GeoInterface.MultiPolygon
-    @test GeoInterface.ngeom(multipolygon) > 0
-
-    A = rand(1:3, i, j)
-    fc = @test_nowarn polygonize(A)
-    @test fc isa GeoInterface.FeatureCollection
-    @test GeoInterface.nfeature(fc) == 3
-    @test map(GeoInterface.getfeature(fc)) do f
-        GeoInterface.properties(f).value
-    end == [1, 2, 3]
+for i in (100, 300), j in (100, 300)
+    @testset "bool arrays without a function return MultiPolygon" begin
+        A = rand(Bool, i, j)
+        @test_nowarn multipolygon = polygonize(A);
+        @test multipolygon isa GeoInterface.MultiPolygon
+        @test GeoInterface.ngeom(multipolygon) > 0
+    end
 
     A = rand(i, j)
-    fc = @test_nowarn polygonize(x -> trunc(3x), A);
-    @test fc isa GeoInterface.FeatureCollection
-    @test GeoInterface.nfeature(fc) == 3
-    @test map(GeoInterface.getfeature(fc)) do f
-        GeoInterface.properties(f).value
-    end == [0.0, 1.0, 2.0]
+    @testset "bool functions return MultiPolygon" begin
+        multipolygon = @test_nowarn polygonize(>(0.5), A);
+        @test multipolygon isa GeoInterface.MultiPolygon
+        @test GeoInterface.ngeom(multipolygon) > 0
+    end
+
+    @testset "other functions return FeatureCollection" begin
+        fc = @test_nowarn polygonize(x -> trunc(3x), A);
+        @test fc isa GeoInterface.FeatureCollection
+        @test GeoInterface.nfeature(fc) == 3
+        @test map(GeoInterface.getfeature(fc)) do f
+            GeoInterface.properties(f).value
+        end == [0.0, 1.0, 2.0]
+    end
+
+    @testset "values are polygonized without a function" begin
+        A = rand(1:3, i, j)
+        fc = @test_nowarn polygonize(A)
+        @test fc isa GeoInterface.FeatureCollection
+        @test GeoInterface.nfeature(fc) == 3
+        @test map(GeoInterface.getfeature(fc)) do f
+            GeoInterface.properties(f).value
+        end == [1, 2, 3]
+    end
 end
