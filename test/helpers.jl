0c5c8894ce4234933c3c0a85d51c61660ae9020b
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-05-16 05:44:33 +0530

    Add a few more GB methods + comments

--- test/helpers.jl
+++ test/helpers.jl
@@ -6,13 +6,22 @@ const TEST_MODULES = [GeoInterface, ArchGDAL, GeometryBasics, LibGEOS]
 # TODO: push this up to GB!
 
 @eval GeometryBasics begin
-    GeometryBasics.geointerface_geomtype(::GeoInterface.LinearRingTrait) = LineString
-    GeometryBasics.geointerface_geomtype(::GeoInterface.LineTrait) = Line
+    # MultiGeometry ncoord implementations
     GeoInterface.ncoord(::GeoInterface.MultiPolygonTrait, ::GeometryBasics.MultiPolygon{N}) where N = N
+    GeoInterface.ncoord(::GeoInterface.MultiLineStringTrait, ::GeometryBasics.MultiLineString{N}) where N = N
+    GeoInterface.ncoord(::GeoInterface.MultiPointTrait, ::GeometryBasics.MultiPoint{N}) where N = N
+    # LinearRing and LineString confusion
+    GeometryBasics.geointerface_geomtype(::GeoInterface.LinearRingTrait) = LineString
+    function GeoInterface.convert(::Type{LineString}, ::GeoInterface.LinearRingTrait, geom)
+        return GeoInterface.convert(LineString, GeoInterface.LineStringTrait(), geom) # forward to the linestring conversion method
+    end
+    # Line interface
+    GeometryBasics.geointerface_geomtype(::GeoInterface.LineTrait) = Line
     function GeoInterface.convert(::Type{Line}, ::GeoInterface.LineTrait, geom)
         p1, p2 = GeoInterface.getpoint(geom)
         return Line(GeoInterface.convert(Point, GeoInterface.PointTrait(), p1), GeoInterface.convert(Point, GeoInterface.PointTrait(), p2))
     end
+    # GeometryCollection interface - currently just a large Union
     const _ALL_GB_GEOM_TYPES = Union{Point, Line, LineString, Polygon, MultiPolygon, MultiLineString, MultiPoint}
     GeometryBasics.geointerface_geomtype(::GeoInterface.GeometryCollectionTrait) = Vector{_ALL_GB_GEOM_TYPES}
     function GeoInterface.convert(::Type{Vector{_ALL_GB_GEOM_TYPES}}, ::GeoInterface.GeometryCollectionTrait, geoms)
