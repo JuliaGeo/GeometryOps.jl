04e1c67acaab7a97f9a68d32808cb497a68f9118
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-08 23:09:30 -0400

    Fix incorrect references

--- ext/GeometryOpsLibGEOSExt/simplify.jl
+++ ext/GeometryOpsLibGEOSExt/simplify.jl
@@ -3,6 +3,6 @@ GO._simplify(::GI.PointTrait, ::GO.TopologyPreserve, geom; kw...) = geom
 GO._simplify(::GI.MultiPointTrait, ::GO.TopologyPreserve, geom; kw...) = geom
 
 function GO._simplify(::GI.AbstractGeometryTrait, alg::GO.TopologyPreserve, geom)
-    return LIBGEOS.topologyPreserveSimplify(GI.convert(LibGEOS, geom), alg.tol)
+    return LG.topologyPreserveSimplify(GI.convert(LG, geom), alg.tol)
 end
 
