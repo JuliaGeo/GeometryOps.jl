d717e8e88181af717f57278d84c7cd8b152edc7f
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-08 22:35:48 -0400

    Add a basic comment to not_implemented_yet.jl

--- src/not_implemented_yet.jl
+++ src/not_implemented_yet.jl
@@ -1,3 +1,8 @@
+# # Not implemented yet
+# All of the functions in this file are not implemented in Julia yet.
+# Some of them may have implementations in LibGEOS which we can use
+# via an extension, but there is no native-Julia implementation for them.
+
 function symdifference end
 function buffer end
 function convexhull end
