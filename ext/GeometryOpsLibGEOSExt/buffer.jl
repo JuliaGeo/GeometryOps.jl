a99d1cada6ddd50c8e55e0bd02544e4fcc2668d8
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-10 19:47:17 -0400

    Set compat for LibGEOS to be after the GeometryCollection patch

--- ext/GeometryOpsLibGEOSExt/buffer.jl
+++ ext/GeometryOpsLibGEOSExt/buffer.jl
@@ -19,11 +19,9 @@ to_join_style(style::LG.GEOSBufJoinStyles) = style
 to_join_style(num::Integer) = num
 
 function GO.buffer(alg::GEOS, geometry, distance)
-    # The reason for this gigantic trait 
-    # LibGEOS.jl cannot convert GeometryCollections yet, so we need to handle the individual geometry types
-    # that can be part of a GeometryCollection.  In the future, once LibGEOS can handle this, we can make the 
-    # trait target simply `AbstractGeometryTrait`.
-    return apply(TraitTarget{Union{GI.AbstractMultiCurveTrait, GI.AbstractMultiPointTrait, GI.AbstractMultiSurfaceTrait, GI.PointTrait, GI.AbstractCurveTrait, GI.AbstractCurvePolygonTrait}}(), geometry) do geom
+    # The reason we use apply here is so that this also works with featurecollections,
+    # tables, vectors of geometries, etc!
+    return apply(TraitTarget{GI.AbstractGeometryTrait}(), geometry) do geom
         LG.bufferWithStyle(
             GI.convert(LG, geom), distance; 
             quadsegs = get(alg, :quadsegs, 8),
