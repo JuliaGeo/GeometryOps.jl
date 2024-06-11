c46c2d871161c9823e0a83d2d5a51cb5e3ae8d35
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-06-10 20:06:29 -0400

    Go back to regular testsets from SafeTestsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,5 @@
 using GeometryOps
-using Test, SafeTestsets
+using Test
 
 using GeometryOps.GeoInterface
 using GeometryOps.GeometryBasics
@@ -14,60 +14,33 @@ const AG = ArchGDAL
 const LG = LibGEOS
 const GO = GeometryOps
 
-# Hack LG to have GeometryCollection converts
-@eval LG begin
-    GI.convert(
-        ::Type{GeometryCollection},
-        ::GeometryCollectionTrait,
-        geom::GeometryCollection;
-        context = nothing,
-    ) = geom
-    function GI.convert(
-        ::Type{GeometryCollection},
-        ::GeometryCollectionTrait,
-        geom;
-        context = get_global_context(),
-    )
-        geometries =
-            [
-                begin
-                    t = GI.trait(g)
-                    lg = geointerface_geomtype(t)
-                    GI.convert(lg, t, g; context) 
-                end
-                for g in GI.getgeom(geom)
-            ]
-        return GeometryCollection(geometries)
-    end
-end
-
 @testset "GeometryOps.jl" begin
-    @safetestset "Primitives" begin include("primitives.jl") end
-    # # # Methods
-    @safetestset "Angles" begin include("methods/angles.jl") end
-    @safetestset "Area" begin include("methods/area.jl") end
-    @safetestset "Barycentric coordinate operations" begin include("methods/barycentric.jl") end
-    @safetestset "Orientation" begin include("methods/orientation.jl") end
-    @safetestset "Centroid" begin include("methods/centroid.jl") end
-    @safetestset "DE-9IM Geom Relations" begin include("methods/geom_relations.jl") end
-    @safetestset "Distance" begin include("methods/distance.jl") end
-    @safetestset "Equals" begin include("methods/equals.jl") end
-    # # # Clipping
-    @safetestset "Coverage" begin include("methods/clipping/coverage.jl") end
-    @safetestset "Cut" begin include("methods/clipping/cut.jl") end
-    @safetestset "Polygon Clipping" begin include("methods/clipping/polygon_clipping.jl") end
-    # # Transformations
-    @safetestset "Embed Extent" begin include("transformations/extent.jl") end
-    @safetestset "Reproject" begin include("transformations/reproject.jl") end
-    @safetestset "Flip" begin include("transformations/flip.jl") end
-    @safetestset "Simplify" begin include("transformations/simplify.jl") end
-    @safetestset "Segmentize" begin include("transformations/segmentize.jl") end
-    @safetestset "Transform" begin include("transformations/transform.jl") end
-    @safetestset "Geometry correction" begin 
+    @testset "Primitives" begin include("primitives.jl") end
+    # Methods
+    @testset "Angles" begin include("methods/angles.jl") end
+    @testset "Area" begin include("methods/area.jl") end
+    @testset "Barycentric coordinate operations" begin include("methods/barycentric.jl") end
+    @testset "Orientation" begin include("methods/orientation.jl") end
+    @testset "Centroid" begin include("methods/centroid.jl") end
+    @testset "DE-9IM Geom Relations" begin include("methods/geom_relations.jl") end
+    @testset "Distance" begin include("methods/distance.jl") end
+    @testset "Equals" begin include("methods/equals.jl") end
+    # Clipping
+    @testset "Coverage" begin include("methods/clipping/coverage.jl") end
+    @testset "Cut" begin include("methods/clipping/cut.jl") end
+    @testset "Polygon Clipping" begin include("methods/clipping/polygon_clipping.jl") end
+    # Transformations
+    @testset "Embed Extent" begin include("transformations/extent.jl") end
+    @testset "Reproject" begin include("transformations/reproject.jl") end
+    @testset "Flip" begin include("transformations/flip.jl") end
+    @testset "Simplify" begin include("transformations/simplify.jl") end
+    @testset "Segmentize" begin include("transformations/segmentize.jl") end
+    @testset "Transform" begin include("transformations/transform.jl") end
+    @testset "Geometry correction" begin 
         include("transformations/correction/geometry_correction.jl")
         include("transformations/correction/closed_ring.jl") 
         include("transformations/correction/intersecting_polygons.jl")
     end
     # Extensions
-    @safetestset "FlexiJoins" begin include("extensions/flexijoins.jl") end
+    @testset "FlexiJoins" begin include("extensions/flexijoins.jl") end
 end
