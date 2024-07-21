ef028e459a99346522355130f5a75b4c8a7a20d0
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-07-21 21:53:38 +0530

    Use DelaunayTriangulation's API to get better results

--- docs/src/experiments/spherical_delaunay_stereographic.jl
+++ docs/src/experiments/spherical_delaunay_stereographic.jl
@@ -15,15 +15,28 @@ using JSON3 # to load data
 using CairoMakie # for plotting
 import Makie: Point3d
 
+# These points are known to be good points, i.e., lon, lat, alt
 points = Point3{Float64}.(JSON3.read(read(Downloads.download("https://gist.githubusercontent.com/Fil/6bc12c535edc3602813a6ef2d1c73891/raw/3ae88bf307e740ddc020303ea95d7d2ecdec0d19/points.json"), String)))
 
 pivot_ind = findfirst(isfinite, points)
+
+# debug
+point_colors = fill(:blue, length(points))
+point_colors[pivot_ind] = :red
+# end debug
+
 pivot_point = points[pivot_ind]
-necessary_rotation = Rotations.RotY(pi) * Rotations.RotY(-pivot_point[2]) * Rotations.RotZ(-pivot_point[1])
+necessary_rotation = Rotations.RotY(-π) * Rotations.RotY(-deg2rad(90-pivot_point[2])) * Rotations.RotZ(-deg2rad(pivot_point[1]))
 
 net_transformation_to_corrected_cartesian = CoordinateTransformations.LinearMap(necessary_rotation) ∘ UnitCartesianFromGeographic()
 
+scatter(map(net_transformation_to_corrected_cartesian, points); color = point_colors)
+#
 stereographic_points = (StereographicFromCartesian() ∘ net_transformation_to_corrected_cartesian).(points)
+scatter(stereographic_points; color = point_colors)
+
+triangulation_points = copy(stereographic_points)
+
 
 # Iterate through the points and find infinite/invalid points
 max_radius = 1
@@ -36,18 +49,48 @@ for (i, point) in enumerate(stereographic_points)
         max_radius = m
     end
 end
+@show max_radius length(really_far_idxs)
 far_value = 1e6 * sqrt(max_radius)
+
 if !isempty(really_far_idxs)
-    stereographic_points[really_far_idxs] .= Point2(far_value, 0)
+    triangulation_points[really_far_idxs] .= Point2(far_value, 0)
 end
 
-# Add infinite horizon points
+boundary_points = reverse([
+    (-far_value, -far_value),
+    (-far_value, far_value),
+    (far_value, far_value),
+    (far_value, -far_value),
+    (-far_value, -far_value),
+])
 triangulation_points = copy(stereographic_points)
-push!(triangulation_points, Point2(0, far_value))
-push!(triangulation_points, Point2(-far_value, 0))
-push!(triangulation_points, Point2(0, -far_value))
 
-triangulation = DelTri.triangulate(triangulation_points)
+boundary_nodes, pts = DelTri.convert_boundary_points_to_indices(boundary_points; existing_points=triangulation_points)
+
+triangulation = DelTri.triangulate(pts; boundary_nodes)
+triplot(triangulation)
+#  for diagnostics, try `fig, ax, sc = triplot(triangulation, show_constrained_edges=true, show_convex_hull=true)`
+
+# First, get all the "solid" faces, ie, faces not attached to boundary nodes
+faces = map(DelTri.each_solid_triangle(triangulation)) do face_tuple
+    CairoMakie.GeometryBasics.TriangleFace(map(face_tuple) do i; i in DelTri.get_boundary_nodes(triangulation) ? pivot_ind : i end)
+end
+for ghost_face in DelTri.each_ghost_triangle(triangulation)
+    push!(faces, CairoMakie.GeometryBasics.TriangleFace(map(ghost_face) do i; i ≤ 0 ? pivot_ind : i end))
+end
+
+wireframe(CairoMakie.GeometryBasics.normal_mesh(UnitCartesianFromGeographic().(points), faces))
+
+_f, _a, _tri_p = triplot(triangulation; axis = (; type = Axis3));
+msh = _tri_p.plots[1].plots[1][1][]
+
+msh3d = GeometryBasics.Mesh(Makie.to_ndim(Point3d, GeometryBasics.coordinates(msh), 0), msh.faces)
+f, a, p = wireframe(msh3d; axis = (; type = LScene));
+p.transformation.transform_func[] = Makie.PointTrans{3}() do point
+    CartesianFromStereographic()(point)
+end
+
+f
 
 triangulation.points[1:end-3] .= stereographic_points
 triangulation.points[end-2:end] .= (stereographic_points[pivot_ind],)
@@ -80,6 +123,15 @@ function (::StereographicFromCartesian)(xyz::AbstractVector)
     @assert length(xyz) == 3 "StereographicFromCartesian expects a 3D Cartesian vector"
     x, y, z = xyz
     return Point2(x/(1-z), y/(1-z))
+end
+
+struct CartesianFromStereographic <: CoordinateTransformations.Transformation
+end
+
+function (::CartesianFromStereographic)(stereographic_point)
+    X, Y = stereographic_point
+    x2y2_1 = X^2 + Y^2 + 1
+    return Point3(2X/x2y2_1, 2Y/x2y2_1, (x2y2_1 - 2)/x2y2_1)
 end
 
 struct UnitCartesianFromGeographic <: CoordinateTransformations.Transformation 
@@ -163,6 +215,9 @@ end
 rotate!(p, Rotations.RotX(π/2))
 rotate!(p, Rotations.RotX(0))
 
-pivot = θϕs[end]
-rotate!(p, )
+pivot_point = θϕs[end]
+necessary_rotation = Rotations.RotY(pi) * Rotations.RotY(-pivot_point[2]) * Rotations.RotZ(-pivot_point[1])
 
+rotate!(p, necessary_rotation)
+
+f
