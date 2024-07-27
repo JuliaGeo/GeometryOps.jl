d8231182a3eb682ee65bdbc842a827fd12e8d78b
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-07-27 17:26:19 +0530

    Fix capitalization

--- ext/GeometryOpsDimensionalDataExt/GeometryOpsDimensionalDataExt.jl
+++ ext/GeometryOpsDimensionalDataExt/GeometryOpsDimensionalDataExt.jl
@@ -11,7 +11,7 @@ function GO.polygonize(A::DD.AbstractDimArray; dims=(DD.X(), DD.Y()), crs=GI.crs
     else
         @warn "`polygonsize` is not possible for `Points` sampling, as polygons cover space by definition. Treating as `Intervals`, but this may not be appropriate"
         map(lookups) do l
-            Dd.intervalbounds(DD.set(l, DD.Intervals()))
+            DD.intervalbounds(DD.set(l, DD.Intervals()))
         end
     end
     GO.polygonize(bounds_vecs..., DD.AbstractDimArray; crs, kw...)
