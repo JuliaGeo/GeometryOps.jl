9a9fde4a92e50e878be4e2bb8849c47157ea47b6
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-01-10 10:48:46 -0100

    Add a test for 2D to 3D

--- test/transformations/transform.jl
+++ test/transformations/transform.jl
@@ -13,3 +13,17 @@ geom = GI.Polygon([GI.LinearRing([(1, 2), (3, 4), (5, 6), (1, 2)]),
     f = CoordinateTransformations.Translation(3.5, 1.5)
     @test GO.transform(f, $geom) == translated
 end
+
+@testset_implementations "transform 2D to 3D" begin
+    flat_points_raw = collect(GO.flatten(GI.PointTrait, geom))
+    flat_points_transformed = map(flat_points_raw) do p
+        (GI.x(p), GI.y(p), hypot(GI.x(p), GI.y(p)))
+    end
+
+    geom_transformed = GO.transform(geom) do p
+        (GI.x(p), GI.y(p), hypot(GI.x(p), GI.y(p)))
+    end
+    @test collect(GO.flatten(GI.PointTrait, geom_transformed)) == flat_points_transformed
+    @test GI.is3d(geom_transformed)
+    @test !GI.ismeasured(geom_transformed)
+end
