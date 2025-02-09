fd55acece402b09a88da38172bbcd368c352ec48
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-02-09 12:00:58 -0500

    comments

--- GeometryOpsCore/src/types/exceptions.jl
+++ GeometryOpsCore/src/types/exceptions.jl
@@ -1,4 +1,12 @@
+#=
+# Errors and exceptions
 
+We create a few custom exception types in this file,
+that have nice show methods that we can use for certain errors.
+
+This makes it substantially easier to catch specific kinds of errors and show them.
+For example 
+=#
 """
     WrongManifoldError{InputManifold, DesiredManifold, Algorithm} <: Error
 
