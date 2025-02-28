159f87392f1977a19a044c728ed045f49b4eada9
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-02-28 00:05:39 -0500

    finish natural index implementation

--- src/utils/SpatialTreeInterface/NaturalIndex.jl
+++ src/utils/SpatialTreeInterface/NaturalIndex.jl
@@ -18,6 +18,7 @@ struct NaturalIndex{E <: Extents.Extent}
 end
 
 GI.extent(idx::NaturalIndex) = idx.extent
+Extents.extent(idx::NaturalIndex) = idx.extent
 
 function Base.show(io::IO, ::MIME"text/plain", idx::NaturalIndex)
     println(io, "NaturalIndex with $(length(idx.levels)) levels and $(idx.nodecapacity) children per node")
@@ -97,6 +98,8 @@ struct NaturalTreeNode{E <: Extents.Extent}
     extent::E
 end
 
+Extents.extent(node::NaturalTreeNode) = node.extent
+
 """
     query(f, index::NaturalIndex, pred)
 
@@ -122,58 +125,153 @@ Otherwise, return the predicate unchanged.
 
 Users and developers may overload this function to provide custom behaviour when something is passed in.
 """
-function sanitize_pred(pred::P) where P
-    sanitize_pred(GI.trait(pred), pred)
+function sanitize_query_predicate(pred::P) where P
+    sanitize_query_predicate(GI.trait(pred), pred)
 end
 
-sanitize_pred(::Nothing, pred::P) where P = pred
-sanitize_pred(::GI.AbstractTrait, pred) = sanitize_pred(GI.extent(pred))
+sanitize_query_predicate(::Nothing, pred::P) where P = pred
+sanitize_query_predicate(::GI.AbstractTrait, pred) = sanitize_pred(GI.extent(pred))
 
-sanitize_pred(pred::Extents.Extent) = Base.Fix1(Extents.intersects, pred)
+sanitize_query_predicate(pred::Extents.Extent) = Base.Fix1(Extents.intersects, pred)
+
+# What does SpatialTreeInterface require of trees?
+# - Parents completely cover their children
+# - `GI.extent(node)` returns `Extent` 
+#   - can mean that `Extents.extent(node)` returns the extent of the node
+# - `nchild(node)` returns the number of children of the node
+# - `getchild(node)` returns an iterator over all children of the node
+# - `getchild(node, i)` returns the i-th child of the node
+# - `isleaf(node)` returns a boolean indicating whether the node is a leaf
+# - `child_indices_extents(node)` returns an iterator over the indices and extents of the children of the node
+
+function nchild(node::NaturalTreeNode)
+    start_idx = (node.index - 1) * node.parent_index.nodecapacity + 1
+    stop_idx = min(start_idx + node.parent_index.nodecapacity - 1, length(node.parent_index.levels[node.level+1].extents))
+    return stop_idx - start_idx + 1
+end
+
+function getchild(node::NaturalTreeNode, i::Int)
+    child_index = (node.index - 1) * node.parent_index.nodecapacity + i
+    return NaturalTreeNode(
+        node.parent_index, 
+        node.level + 1, # increment level by 1
+        child_index, # index of this particular child
+        node.parent_index.levels[node.level+1].extents[child_index] # the extent of this child
+    )
+end
+
+# Get all children of a node
+function getchild(node::NaturalTreeNode)
+    return (getchild(node, i) for i in 1:nchild(node))
+end
+
+isleaf(node::NaturalTreeNode) = node.level == length(node.parent_index.levels) - 1
+
+function child_indices_extents(node::NaturalTreeNode)
+    start_idx = (node.index - 1) * node.parent_index.nodecapacity + 1
+    stop_idx = min(start_idx + node.parent_index.nodecapacity - 1, length(node.parent_index.levels[node.level+1].extents))
+    return ((i, node.parent_index.levels[node.level+1].extents[i]) for i in start_idx:stop_idx)
+end
+
+# implementation for "root node" / top level tree
+
+isleaf(node::NaturalIndex) = length(node.levels) == 1
+
+nchild(node::NaturalIndex) = length(node.levels[1].extents)
+
+getchild(node::NaturalIndex) = getchild(NaturalTreeNode(node, 0, 1, node.extent))
+getchild(node::NaturalIndex, i) = getchild(NaturalTreeNode(node, 0, 1, node.extent), i)
+
+child_indices_extents(node::NaturalIndex) = (i_ext for i_ext in enumerate(node.levels[1].extents))
 
 
-function query(f, index::NaturalIndex, pred)
-    # pred = sanitize_pred(ipred)
-    # At the top level, iterate over all toplevel nodes and query the extent
-    first_level = index.levels[1]
-    for (node_idx, node_extent) in enumerate(first_level.extents)
-        if pred(node_extent)
-            query(f, NaturalTreeNode(index, 1, node_idx, node_extent), pred)
+"""
+    do_query(f, predicate, tree)
+
+Call `f(i)` for each index `i` in the tree that satisfies `predicate(extent(i))`.
+
+This is generic to anything that implements the SpatialTreeInterface, particularly the methods
+[`isleaf`](@ref), [`getchild`](@ref), and [`child_extents`](@ref).
+"""
+function do_query(f::F, predicate::P, node::N) where {F, P, N}
+    if isleaf(node)
+        for (i, leaf_geometry_extent) in child_indices_extents(node)
+            if predicate(leaf_geometry_extent)
+                @controlflow f(i)
+            end
+        end
+    else
+        for child in getchild(node)
+            if predicate(GI.extent(child))
+                do_query(f, predicate, child)
+            end
         end
     end
 end
 
-function query(f, node::NaturalTreeNode, pred)
-    # pred = sanitize_pred(ipred)
-    # If we're at the bottom level, iterate over all geometries in the node
-    # while performing extent rejection
-    if node.level == length(node.parent_index.levels) - 1
-        
-        extents = node.parent_index.levels[end].extents
-        # At the leaf level, indices are equivalent to the geometry index
-        start_idx = (node.index - 1) * node.parent_index.nodecapacity + 1
-        stop_idx = min(start_idx + node.parent_index.nodecapacity - 1, length(extents))
-        
-        for i in start_idx:stop_idx
-            if pred(extents[i])
-                # The @controlflow macro allows `f` to return an `Action`
-                # from LoopStateMachine.jl, which allows us to break out of the loop, 
-                # or return a value directly if we wish.
-                @controlflow f(i) # provide `f` the index of the geometry in question
+function do_query(predicate, node)
+    a = Int[]
+    do_query(Base.Fix1(push!, a), predicate, node)
+    return a
+end
+
+# implement spatial tree interface for SortTileRecursiveTree.jl
+
+nchild(tree::STRtree) = nchild(tree.rootnode)
+getchild(tree::STRtree) = getchild(tree.rootnode)
+getchild(tree::STRtree, i) = getchild(tree.rootnode, i)
+isleaf(tree::STRtree) = isleaf(tree.rootnode)
+child_indices_extents(tree::STRtree) = child_indices_extents(tree.rootnode)
+
+
+nchild(node::STRNode) = length(node.children)
+getchild(node::STRNode) = node.children
+getchild(node::STRNode, i) = node.children[i]
+isleaf(node::STRNode) = false # STRNodes are not leaves by definition
+
+isleaf(node::STRLeafNode) = true
+child_indices_extents(node::STRLeafNode) = zip(node.indices, node.extents)
+
+# now a `do_query` function call "just works"!
+
+
+"""
+    do_dual_query(f, predicate, node1, node2)
+
+Call `f(i1, i2)` for each index `i1` in `node1` and `i2` in `node2` that satisfies `predicate(extent(i1), extent(i2))`.
+
+This is generic to anything that implements the SpatialTreeInterface, particularly the methods
+[`isleaf`](@ref), [`getchild`](@ref), and [`child_extents`](@ref).
+"""
+function do_dual_query(f::F, predicate::P, node1::N1, node2::N2) where {F, P, N1, N2}
+    if isleaf(node1) && isleaf(node2)
+        # both nodes are leaves, so we can just iterate over the indices and extents
+        for (i1, extent1) in child_indices_extents(node1)
+            for (i2, extent2) in child_indices_extents(node2)
+                if predicate(extent1, extent2)
+                    @controlflow f(i1, i2)
+                end
             end
         end
-    else # not a leaf node, recurse lower down the tree
-
-        extents = node.parent_index.levels[node.level+1].extents
-
-        start_idx = (node.index - 1) * node.parent_index.nodecapacity + 1
-        stop_idx = min(start_idx + node.parent_index.nodecapacity - 1, length(extents))
-        
-        for i in start_idx:stop_idx
-            if pred(extents[i])
-                query(f, NaturalTreeNode(node.parent_index, node.level + 1, i, extents[i]), pred)
+    elseif isleaf(node1) # node2 is not a leaf, node1 is - recurse further into node2
+        for child in getchild(node2)
+            if predicate(GI.extent(node1), GI.extent(child))
+                do_dual_query(f, predicate, node1, child)
+            end
+        end
+    elseif isleaf(node2) # node1 is not a leaf, node2 is - recurse further into node1
+        for child in getchild(node1)
+            if predicate(GI.extent(child), GI.extent(node2))
+                do_dual_query(f, predicate, child, node2)
+            end
+        end
+    else # neither node is a leaf, recurse into both children
+        for child1 in getchild(node1)
+            for child2 in getchild(node2)
+                if predicate(GI.extent(child1), GI.extent(child2))
+                    do_dual_query(f, predicate, child1, child2)
+                end
             end
         end
     end
-
 end
