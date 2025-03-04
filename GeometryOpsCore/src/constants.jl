fc2d7b8c021d9dcf9ae65700d449d0906636f0f0
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-03-03 19:21:12 -0500

    Move all constants to a named constants file, with docstrings

---
+++ GeometryOpsCore/src/constants.jl
@@ -0,0 +1,8 @@
+"The semi-major axis of the WGS84 ellipsoid"
+const WGS84_EARTH_SEMI_MAJOR_RADIUS = 6378137.0
+
+"The inverse flattening of the WGS84 ellipsoid"
+const WGS84_EARTH_INV_FLATTENING = 298.257223563
+
+"The mean radius of the WGS84 ellipsoid, used for spherical manifold default"
+const WGS84_EARTH_MEAN_RADIUS = 6371008.8
