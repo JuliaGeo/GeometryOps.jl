2ca2a3c411379b2d457c4b23c0bafbd05331b920
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-05-02 11:52:37 -0400

    document init

--- GeometryOpsCore/src/applyreduce.jl
+++ GeometryOpsCore/src/applyreduce.jl
@@ -51,7 +51,7 @@ Literate.jl source code is below.
 =#
 
 """
-    applyreduce(f, op, target::Union{TraitTarget, GI.AbstractTrait}, obj; threaded)
+    applyreduce(f, op, target::Union{TraitTarget, GI.AbstractTrait}, obj; threaded, init, kw...)
 
 Apply function `f` to all objects with the `target` trait,
 and reduce the result with an `op` like `+`. 
@@ -60,6 +60,8 @@ The order and grouping of application of `op` is not guaranteed.
 
 If `threaded==true` threads will be used over arrays and iterables, 
 feature collections and nested geometries.
+
+`init` functions the same way as it does in base Julia functions like `reduce`.
 """
 @inline function applyreduce(
     f::F, op::O, target, geom; threaded=false, init=nothing
