709150c162499a64adb22133928bb0875fb99cf6
Documenter.jl <documenter@juliadocs.github.io>
2025-09-10 21:06:49 +0000

    build based on 518369e

--- previews/PR240/assets/explanations_crs.md.BiUUAhZt.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as a}from"./chunks/framework.B4tCK4xg.js";const _=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"explanations/crs.md","filePath":"explanations/crs.md","lastUpdated":null}'),s={name:"explanations/crs.md"};function n(r,o,c,p,i,l){return a(),t("div")}const m=e(s,[["render",n]]);export{_ as __pageData,m as default};
