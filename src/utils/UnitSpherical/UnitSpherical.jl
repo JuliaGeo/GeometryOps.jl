7b71a1157726ac8957ab65bcdea0e77930dced07
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-09-15 15:28:45 +0200

    Use robust_cross_product in spherical things

--- src/utils/UnitSpherical/UnitSpherical.jl
+++ src/utils/UnitSpherical/UnitSpherical.jl
@@ -9,16 +9,17 @@ import Random
 # using TestItems # this is a thin package that allows TestItems.@testitem to be parsed.
 
 include("point.jl")
-include("coordinate_transforms.jl")
-include("slerp.jl")
-include("cap.jl")
+
 include("robustcrossproduct/RobustCrossProduct.jl")
-
-export UnitSphericalPoint, UnitSphereFromGeographic, GeographicFromUnitSphere, 
-       slerp, SphericalCap, spherical_distance
-
 # Re-export from RobustCrossProduct
 using .RobustCrossProduct: robust_cross_product
 export robust_cross_product
+
+include("coordinate_transforms.jl")
+include("slerp.jl")
+include("cap.jl")
+
+export UnitSphericalPoint, UnitSphereFromGeographic, GeographicFromUnitSphere, 
+       slerp, SphericalCap, spherical_distance
 
 end
