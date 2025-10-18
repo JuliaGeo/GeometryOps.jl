aa955cb550d5e25b1290acbb4e0cd4ca04f18105
Rafael Schouten <rafaelschouten@gmail.com>
2025-10-18 21:04:58 +1100

    bugfix

--- src/transformations/smooth.jl
+++ src/transformations/smooth.jl
@@ -42,54 +42,55 @@ _smooth(alg, ::GI.PointTrait, geom) = geom
 _smooth(alg, ::GI.MultiPointTrait, geom) = geom
 
 function _smooth(alg::Chaikin, T::Union{GI.LineStringTrait,GI.LinearRingTrait}, geom)
+    isring = T isa GI.LinearRingTrait
     points = tuple_points(geom)
-    needs_ends = T isa GI.LineStringTrait
-    smoothed_points = _chaikin_smooth(points, alg.iterations, needs_ends)
+    if isring && first(points) != last(points)
+        push!(points, first(points))
+    end
+    smoothed_points = _chaikin_smooth(points, alg.iterations, isring)
     return rebuild(geom, smoothed_points)
 end
 
-function _chaikin_smooth(points::Vector{P}, iterations::Int, needs_ends::Bool) where P
+function _chaikin_smooth(points::Vector{P}, iterations::Int, isring::Bool) where P
     # points is expected to be a vector of points
-    num_points = length(points)
-    if num_points < 2
-        return points
-    end
-
     smoothed_points = points
-    for _ in 1:iterations
-        if needs_ends 
+    for itr in 1:iterations
+        num_points = length(smoothed_points)
+        if isring 
+            n = 1
+            new_points = Vector{P}(undef, num_points * 2 - 1)
+        else
             n = 2
             # Need to add the first point
             new_points = Vector{P}(undef, num_points * 2)
-            new_points[1] = first(points)
-        else
-            n = 1
-            new_points = Vector{P}(undef, num_points * 2 - 2)
+            new_points[begin] = smoothed_points[begin]
+            new_points[end] = smoothed_points[end]
         end
+        fill!(new_points, (-9999.0, -9999.0))
 
-        for i in 1:num_points - 1
+        for i in eachindex(smoothed_points)[begin:end-1]
             p1 = smoothed_points[i]
             p2 = smoothed_points[i+1]
-            
-            q_x = 0.75 * GI.x(p1) + 0.25 * GI.x(p2)
-            q_y = 0.75 * GI.y(p1) + 0.25 * GI.y(p2)
-            r_x = 0.25 * GI.x(p1) + 0.75 * GI.x(p2)
-            r_y = 0.25 * GI.y(p1) + 0.75 * GI.y(p2)
-
-            new_points[n] = (q_x, q_y)
-            new_points[n+1] = (r_x, r_y)
+            _add_smoothed_points!(new_points, p1, p2, n)
             n += 2
         end
-
-        if needs_ends
-            # for open curves, we need to add the first and last points
-            # but the loop above does not do that.
-            # instead we can just add them here
-            new_points[end] = points[end]
+ 
+        if isring # Close it
+            new_points[end] = new_points[begin]
         end
 
         smoothed_points = new_points
     end
 
     return smoothed_points
+end
+
+function _add_smoothed_points!(new_points, p1, p2, n)
+    q_x = 0.75 * GI.x(p1) + 0.25 * GI.x(p2)
+    q_y = 0.75 * GI.y(p1) + 0.25 * GI.y(p2)
+    r_x = 0.25 * GI.x(p1) + 0.75 * GI.x(p2)
+    r_y = 0.25 * GI.y(p1) + 0.75 * GI.y(p2)
+
+    new_points[n] = (q_x, q_y)
+    new_points[n+1] = (r_x, r_y)
 end
