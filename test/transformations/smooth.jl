0bd3def427c1524d08c236626fbd0b1d6f0ecdd7
Anshul Singhvi <anshulsinghvi@gmail.com>
2025-10-18 09:09:33 -0400

    Refactor tests to be a bit easier to parse

--- test/transformations/smooth.jl
+++ test/transformations/smooth.jl
@@ -5,45 +5,36 @@ import GeoInterface as GI
 @testset "Chaikin" begin
     @testset "LineString" begin
         line = GI.LineString([(0.0, 0.0), (1.0, 1.0), (2.0, 0.0)])
+        expected = [(0.0, 0.0), (0.25, 0.25), (0.75, 0.75), (1.25, 0.75), (1.75, 0.25), (2.0, 0.0)]
         smoothed = GO.smooth(line)
-        @test GI.npoint(smoothed) == 6
-        @test GI.getpoint(smoothed, 1) == (0.0, 0.0)
-        @test GI.getpoint(smoothed, 2) == (0.25, 0.25)
-        @test GI.getpoint(smoothed, 3) == (0.75, 0.75)
-        @test GI.getpoint(smoothed, 4) == (1.25, 0.75)
-        @test GI.getpoint(smoothed, 5) == (1.75, 0.25)
-        @test GI.getpoint(smoothed, 6) == (2.0, 0.0)
+        @test GI.npoint(smoothed) == length(expected)
+        for i in 1:length(expected)
+            @testset let i = i
+                @test GI.getpoint(smoothed, i) == expected[i]
+            end
+        end
 
         smoothed = GO.smooth(line; iterations=2)
         @test GI.npoint(smoothed) == 12
-        @test GI.getpoint(smoothed, 1) == (0.0, 0.0)
-        @test GI.getpoint(smoothed, 2) == (0.0625, 0.0625)
-        @test GI.getpoint(smoothed, 3) == (0.1875, 0.1875)
-        @test GI.getpoint(smoothed, 4) == (0.375, 0.375)
-        @test GI.getpoint(smoothed, 5) == (0.625, 0.625)
-        @test GI.getpoint(smoothed, 6) == (0.875, 0.75)
-        @test GI.getpoint(smoothed, 7) == (1.125, 0.75)
-        @test GI.getpoint(smoothed, 8) == (1.375, 0.625)
-        @test GI.getpoint(smoothed, 9) == (1.625, 0.375)
-        @test GI.getpoint(smoothed, 10) == (1.8125, 0.1875)
-        @test GI.getpoint(smoothed, 11) == (1.9375, 0.0625)
-        @test GI.getpoint(smoothed, 12) == (2.0, 0.0)
+        expected = [(0.0, 0.0), (0.0625, 0.0625), (0.1875, 0.1875), (0.375, 0.375), (0.625, 0.625), (0.875, 0.75), (1.125, 0.75), (1.375, 0.625), (1.625, 0.375), (1.8125, 0.1875), (1.9375, 0.0625), (2.0, 0.0)]
+        for i in 1:length(expected)
+            @testset let i = i
+                @test GI.getpoint(smoothed, i) == expected[i]
+            end
+        end
     end
 
     @testset "Polygon" begin
         poly = GI.Polygon([[(0.0, 0.0), (1.0, 0.0), (1.0, 1.0), (0.0, 1.0), (0.0, 0.0)]])
         smoothed = GO.smooth(poly)
         exterior = GI.getexterior(smoothed)
-        @test GI.npoint(exterior) == 9
-        @test GI.getpoint(exterior, 1) == (0.25, 0.0)
-        @test GI.getpoint(exterior, 2) == (0.75, 0.0)
-        @test GI.getpoint(exterior, 3) == (1.0, 0.25)
-        @test GI.getpoint(exterior, 4) == (1.0, 0.75)
-        @test GI.getpoint(exterior, 5) == (0.75, 1.0)
-        @test GI.getpoint(exterior, 6) == (0.25, 1.0)
-        @test GI.getpoint(exterior, 7) == (0.0, 0.75)
-        @test GI.getpoint(exterior, 8) == (0.0, 0.25)
-        @test GI.getpoint(exterior, 9) == (0.25, 0.0)
+        expected = [(0.25, 0.0), (0.75, 0.0), (1.0, 0.25), (1.0, 0.75), (0.75, 1.0), (0.25, 1.0), (0.0, 0.75), (0.0, 0.25), (0.25, 0.0)]
+        @test GI.npoint(exterior) == length(expected)
+        for i in 1:length(expected)
+            @testset let i = i
+                @test GI.getpoint(exterior, i) == expected[i]
+            end
+        end
     end
 
     @testset "MultiPolygon" begin
