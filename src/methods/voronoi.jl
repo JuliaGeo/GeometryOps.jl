85627f08634214cb6592a1d013514211754665d8
Skylar Gering <sgering@mit.edu>
2025-11-02 21:06:08 -0500

    Pare down doctests

--- src/methods/voronoi.jl
+++ src/methods/voronoi.jl
@@ -88,7 +88,7 @@ GO.voronoi(points; rng = rng)
 ```jldoctest voronoi
 clip_points = ((0.0,0.0), (5.0,0.0), (5.0,5.0), (0.0,5.0), (0.0,0.0))
 clip_order = (1, 2, 3, 4, 1)
-clip_poly1 = GI.Polygon([collect(clip_points)])
+clip_poly1 = (clip_points, clip_order) # tuples
 GO.voronoi(points; clip_polygon = clip_poly1, rng = rng)
 # output
 3-element Vector{GeoInterface.Wrappers.Polygon{false, false, Vector{GeoInterface.Wrappers.LinearRing{false, false, Vector{Tuple{Float64, Float64}}, Nothing, Nothing}}, Nothing, Nothing}}:
@@ -97,20 +97,10 @@ GO.voronoi(points; clip_polygon = clip_poly1, rng = rng)
  GeoInterface.Wrappers.Polygon{false, false}([GeoInterface.Wrappers.LinearRing([(0.0, 5.0), … (3) … , (0.0, 5.0)])])
 ````
 
+
 ```jldoctest voronoi
-clip_poly2 = (clip_points, clip_order) # tuples
+clip_poly2 = (collect(clip_points), collect(clip_order)) # vectors
 GO.voronoi(points; clip_polygon = clip_poly2, rng = rng)
-# output
-3-element Vector{GeoInterface.Wrappers.Polygon{false, false, Vector{GeoInterface.Wrappers.LinearRing{false, false, Vector{Tuple{Float64, Float64}}, Nothing, Nothing}}, Nothing, Nothing}}:
- GeoInterface.Wrappers.Polygon{false, false}([GeoInterface.Wrappers.LinearRing([(5.0, 0.0), … (3) … , (5.0, 0.0)])])
- GeoInterface.Wrappers.Polygon{false, false}([GeoInterface.Wrappers.LinearRing([(3.7328227614527916, 0.0), … (3) … , (3.7328227614527916, 0.0)])])
- GeoInterface.Wrappers.Polygon{false, false}([GeoInterface.Wrappers.LinearRing([(0.0, 5.0), … (3) … , (0.0, 5.0)])])
-````
-
-
-```jldoctest voronoi
-clip_poly3 = (collect(clip_points), collect(clip_order)) # vectors
-GO.voronoi(points; clip_polygon = clip_poly3, rng = rng)
 # output
 3-element Vector{GeoInterface.Wrappers.Polygon{false, false, Vector{GeoInterface.Wrappers.LinearRing{false, false, Vector{Tuple{Float64, Float64}}, Nothing, Nothing}}, Nothing, Nothing}}:
  GeoInterface.Wrappers.Polygon{false, false}([GeoInterface.Wrappers.LinearRing([(5.0, 0.0), … (3) … , (5.0, 0.0)])])
