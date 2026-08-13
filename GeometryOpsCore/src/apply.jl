JuliaGeo/GeometryOps.jl

commit 249cef4a5e376a9b025ac9b42aec88311c9e4e6e

Sg/calc angles (#50)
* Add angle code

* Finish angle code

* Add tests back in

* Change hole angles to interior

* Fix angle docs

* Add multipoints

* Update doc manifest

* Update makedocs keywords

* Add documenter source code to gitignore

* Update comment examples to find error

* More comment example fixes

* More document fixes

docs/make.jl
@@ -50,7 +50,7 @@ makedocs(;
         "Home" => "index.md",
         "Source code" => literate_pages
     ],
-    strict=false,
+    warnonly = true,
 )
 
 deploydocs(;

src/methods/barycentric.jl
@@ -24,88 +24,90 @@ export MeanValue
 
 # ## Example
 # This example was taken from [this page of CGAL's documentation](https://doc.cgal.org/latest/Barycentric_coordinates_2/index.html).
+#=
+```@example barycentric
+import GeometryOps as GO
+using GeometryOps.GeometryBasics
+using Makie
+using CairoMakie
+# Define a polygon
+polygon_points = Point3f[
+(0.03, 0.05, 0.00), (0.07, 0.04, 0.02), (0.10, 0.04, 0.04),
+(0.14, 0.04, 0.06), (0.17, 0.07, 0.08), (0.20, 0.09, 0.10),
+(0.22, 0.11, 0.12), (0.25, 0.11, 0.14), (0.27, 0.10, 0.16),
+(0.30, 0.07, 0.18), (0.31, 0.04, 0.20), (0.34, 0.03, 0.22),
+(0.37, 0.02, 0.24), (0.40, 0.03, 0.26), (0.42, 0.04, 0.28),
+(0.44, 0.07, 0.30), (0.45, 0.10, 0.32), (0.46, 0.13, 0.34),
+(0.46, 0.19, 0.36), (0.47, 0.26, 0.38), (0.47, 0.31, 0.40),
+(0.47, 0.35, 0.42), (0.45, 0.37, 0.44), (0.41, 0.38, 0.46),
+(0.38, 0.37, 0.48), (0.35, 0.36, 0.50), (0.32, 0.35, 0.52),
+(0.30, 0.37, 0.54), (0.28, 0.39, 0.56), (0.25, 0.40, 0.58),
+(0.23, 0.39, 0.60), (0.21, 0.37, 0.62), (0.21, 0.34, 0.64),
+(0.23, 0.32, 0.66), (0.24, 0.29, 0.68), (0.27, 0.24, 0.70),
+(0.29, 0.21, 0.72), (0.29, 0.18, 0.74), (0.26, 0.16, 0.76),
+(0.24, 0.17, 0.78), (0.23, 0.19, 0.80), (0.24, 0.22, 0.82),
+(0.24, 0.25, 0.84), (0.21, 0.26, 0.86), (0.17, 0.26, 0.88),
+(0.12, 0.24, 0.90), (0.07, 0.20, 0.92), (0.03, 0.15, 0.94),
+(0.01, 0.10, 0.97), (0.02, 0.07, 1.00)]
+# Plot it!
+# First, we'll plot the polygon using Makie's rendering:
+f, a1, p1 = poly(
+    polygon_points; 
+    color = last.(polygon_points), colormap = cgrad(:jet, 18; categorical = true), 
+    axis = (; 
+        aspect = DataAspect(), title = "Makie mesh based polygon rendering", subtitle = "CairoMakie"
+    ), 
+    figure = (; resolution = (800, 400),)
+)
 
-# ```@example barycentric
-# using GeometryOps, Makie
-# using GeometryOps.GeometryBasics
-# # Define a polygon
-# polygon_points = Point3f[
-# (0.03, 0.05, 0.00), (0.07, 0.04, 0.02), (0.10, 0.04, 0.04),
-# (0.14, 0.04, 0.06), (0.17, 0.07, 0.08), (0.20, 0.09, 0.10),
-# (0.22, 0.11, 0.12), (0.25, 0.11, 0.14), (0.27, 0.10, 0.16),
-# (0.30, 0.07, 0.18), (0.31, 0.04, 0.20), (0.34, 0.03, 0.22),
-# (0.37, 0.02, 0.24), (0.40, 0.03, 0.26), (0.42, 0.04, 0.28),
-# (0.44, 0.07, 0.30), (0.45, 0.10, 0.32), (0.46, 0.13, 0.34),
-# (0.46, 0.19, 0.36), (0.47, 0.26, 0.38), (0.47, 0.31, 0.40),
-# (0.47, 0.35, 0.42), (0.45, 0.37, 0.44), (0.41, 0.38, 0.46),
-# (0.38, 0.37, 0.48), (0.35, 0.36, 0.50), (0.32, 0.35, 0.52),
-# (0.30, 0.37, 0.54), (0.28, 0.39, 0.56), (0.25, 0.40, 0.58),
-# (0.23, 0.39, 0.60), (0.21, 0.37, 0.62), (0.21, 0.34, 0.64),
-# (0.23, 0.32, 0.66), (0.24, 0.29, 0.68), (0.27, 0.24, 0.70),
-# (0.29, 0.21, 0.72), (0.29, 0.18, 0.74), (0.26, 0.16, 0.76),
-# (0.24, 0.17, 0.78), (0.23, 0.19, 0.80), (0.24, 0.22, 0.82),
-# (0.24, 0.25, 0.84), (0.21, 0.26, 0.86), (0.17, 0.26, 0.88),
-# (0.12, 0.24, 0.90), (0.07, 0.20, 0.92), (0.03, 0.15, 0.94),
-# (0.01, 0.10, 0.97), (0.02, 0.07, 1.00)]
-# # Plot it!
-# # First, we'll plot the polygon using Makie's rendering:
-# f, a1, p1 = poly(
-#     polygon_points; 
-#     color = last.(polygon_points), colormap = cgrad(:jet, 18; categorical = true), 
-#     axis = (; 
-#         aspect = DataAspect(), title = "Makie mesh based polygon rendering", subtitle = "CairoMakie"
-#     ), 
-#     figure = (; resolution = (800, 400),)
-# )
-#
-# Makie.update_state_before_display!(f) # We have to call this explicitly, to get the axis limits correct
-# # Now that we've plotted the first polygon,
-# # we can render it using barycentric coordinates.
-# a1_bbox = a1.finallimits[] # First we get the extent of the axis
-# ext = GeometryOps.GI.Extent(NamedTuple{(:X, :Y)}(zip(minimum(a1_bbox), maximum(a1_bbox))))
-#
-# a2, p2box = poly( # Now, we plot a cropping rectangle around the axis so we only show the polygon
-#     f[1, 2], 
-#     GeometryOps.GeometryBasics.Polygon( # This is a rectangle with an internal hole shaped like the polygon.
-#         Point2f[(ext.X[1], ext.Y[1]), (ext.X[2], ext.Y[1]), (ext.X[2], ext.Y[2]), (ext.X[1], ext.Y[2]), (ext.X[1], ext.Y[1])], 
-#         [reverse(Point2f.(polygon_points))]
-#     ); 
-#     color = :white, xautolimits = false, yautolimits = false,
-#     axis = (; 
-#         aspect = DataAspect(), title = "Barycentric coordinate based polygon rendering", subtitle = "GeometryOps",
-#         limits = (ext.X, ext.Y), 
-#     )
-# )
-# hidedecorations!(a1)
-# hidedecorations!(a2)
-# cb = Colorbar(f[2, :], p1.plots[1]; vertical = false, flipaxis = true)
-# # Finally, we perform barycentric interpolation on a grid,
-# xrange = LinRange(ext.X..., widths(a2.scene.px_area[])[1] * 4) # 2 rendered pixels per "physical" pixel
-# yrange = LinRange(ext.Y..., widths(a2.scene.px_area[])[2] * 4) # 2 rendered pixels per "physical" pixel
-# @time mean_values = barycentric_interpolate.(
-#     (MeanValue(),), # The barycentric coordinate algorithm (MeanValue is the only one for now)
-#     (Point2f.(polygon_points),), # The polygon points as `Point2f`
-#     (last.(polygon_points,),),   # The values per polygon point - can be anything which supports addition and division
-#     Point2f.(xrange, yrange')    # The points at which to interpolate
-# )
-# # and render!
-# hm = heatmap!(
-#     a2, xrange, yrange, mean_values;
-#     colormap = p1.colormap, # Use the same colormap as the original polygon plot
-#     colorrange = p1.plots[1].colorrange[], # Access the rendered mesh plot's colorrange directly
-#     transformation = (; translation = Vec3f(0,0,-1)), # This gets the heatmap to render "behind" the previously plotted polygon
-#     xautolimits = false, yautolimits = false
-# )
-# f
-# ```
+Makie.update_state_before_display!(f) # We have to call this explicitly, to get the axis limits correct
+# Now that we've plotted the first polygon,
+# we can render it using barycentric coordinates.
+a1_bbox = a1.finallimits[] # First we get the extent of the axis
+ext = GeometryOps.GI.Extent(NamedTuple{(:X, :Y)}(zip(minimum(a1_bbox), maximum(a1_bbox))))
 
-# ## Barycentric-coordinate API
-# In some cases, we actually want barycentric interpolation, and have no interest 
-# in the coordinates themselves.  
-# 
-# However, the coordinates can be useful for debugging, and when performing 3D rendering,
-# multiple barycentric values (depth, uv) are needed for depth buffering.
-#
+a2, p2box = poly( # Now, we plot a cropping rectangle around the axis so we only show the polygon
+    f[1, 2], 
+    GeometryOps.GeometryBasics.Polygon( # This is a rectangle with an internal hole shaped like the polygon.
+        Point2f[(ext.X[1], ext.Y[1]), (ext.X[2], ext.Y[1]), (ext.X[2], ext.Y[2]), (ext.X[1], ext.Y[2]), (ext.X[1], ext.Y[1])], 
+        [reverse(Point2f.(polygon_points))]
+    ); 
+    color = :white, xautolimits = false, yautolimits = false,
+    axis = (; 
+        aspect = DataAspect(), title = "Barycentric coordinate based polygon rendering", subtitle = "GeometryOps",
+        limits = (ext.X, ext.Y), 
+    )
+)
+hidedecorations!(a1)
+hidedecorations!(a2)
+cb = Colorbar(f[2, :], p1.plots[1]; vertical = false, flipaxis = true)
+# Finally, we perform barycentric interpolation on a grid,
+xrange = LinRange(ext.X..., widths(a2.scene.px_area[])[1] * 4) # 2 rendered pixels per "physical" pixel
+yrange = LinRange(ext.Y..., widths(a2.scene.px_area[])[2] * 4) # 2 rendered pixels per "physical" pixel
+@time mean_values = barycentric_interpolate.(
+    (MeanValue(),), # The barycentric coordinate algorithm (MeanValue is the only one for now)
+    (Point2f.(polygon_points),), # The polygon points as `Point2f`
+    (last.(polygon_points,),),   # The values per polygon point - can be anything which supports addition and division
+    Point2f.(xrange, yrange')    # The points at which to interpolate
+)
+# and render!
+hm = heatmap!(
+    a2, xrange, yrange, mean_values;
+    colormap = p1.colormap, # Use the same colormap as the original polygon plot
+    colorrange = p1.plots[1].colorrange[], # Access the rendered mesh plot's colorrange directly
+    transformation = (; translation = Vec3f(0,0,-1)), # This gets the heatmap to render "behind" the previously plotted polygon
+    xautolimits = false, yautolimits = false
+)
+f
+```
+
+## Barycentric-coordinate API
+In some cases, we actually want barycentric interpolation, and have no interest 
+in the coordinates themselves.  
+
+However, the coordinates can be useful for debugging, and when performing 3D rendering,
+multiple barycentric values (depth, uv) are needed for depth buffering.
+=#
 
 const _VecTypes = Union{Tuple{Vararg{T, N}}, GeometryBasics.StaticArraysCore.StaticArray{Tuple{N}, T, 1}} where {N, T}
 

.gitignore
@@ -1,3 +1,4 @@
 /Manifest.toml
 /docs/build/
+/docs/src/source/
 .vscode/

src/GeometryOps.jl
@@ -21,6 +21,7 @@ const Edge = Tuple{TuplePoint,TuplePoint}
 include("primitives.jl")
 include("utils.jl")
 
+include("methods/angles.jl")
 include("methods/area.jl")
 include("methods/barycentric.jl")
 include("methods/bools.jl")

src/methods/angles.jl
@@ -1,0 +1,172 @@
+# # Angles
+export angles
+
+#=
+## What is angles?
+
+Angles are the angles formed by a given geometries line segments, if it has line segments.
+
+To provide an example, consider this rectangle:
+```@example angles
+import GeometryOps as GO
+import GeoInterface as GI
+using Makie, CairoMakie
+
+rect = GI.Polygon([[(0.0, 0.0), (0.0, 1.0), (1.0, 1.0), (1.0, 0.0), (0.0, 0.0)]])
+f, a, p = poly(collect(GI.getpoint(rect)); axis = (; aspect = DataAspect()))
+```
+This is clearly a rectangle, with angles of 90 degrees.
+```@example angles
+GO.angles(rect)  # [90, 90, 90, 90]
+```
+
+## Implementation
+
+This is the GeoInterface-compatible implementation. First, we implement a
+wrapper method that dispatches to the correct implementation based on the
+geometry trait. This is also used in the implementation, since it's a lot less
+work!
+=#
+
+const _ANGLE_TARGETS = Union{GI.PolygonTrait,GI.AbstractCurveTrait,GI.MultiPointTrait,GI.PointTrait}
+
+"""
+    angles(geom, ::Type{T} = Float64)
+
+Returns the angles of a geometry or collection of geometries. 
+This is computed differently for different geometries:
+
+    - The angles of a point is an empty vector.
+    - The angles of a single line segment is an empty vector.
+    - The angles of a linestring or linearring is a vector of angles formed by the curve.
+    - The angles of a polygin is a vector of vectors of angles formed by each ring.
+    - The angles of a multi-geometry collection is a vector of the angles of each of the
+        sub-geometries as defined above.
+
+Result will be a Vector, or nested set of vectors, of type T where an optional argument with
+a default value of Float64.
+"""
+function angles(geom, ::Type{T} = Float64; threaded =false) where T <: AbstractFloat
+    applyreduce(vcat, _ANGLE_TARGETS, geom; threaded, init = Vector{T}()) do g
+        _angles(T, GI.trait(g), g)
+    end
+end
+
+# Points and single line segments have no angles
+_angles(::Type{T}, ::Union{GI.PointTrait, GI.MultiPointTrait, GI.LineTrait}, geom) where T = T[]
+
+#= The angles of a linestring are the angles formed by the line. If the first and last point
+are not explicitly repeated, the geom is not considered closed. The angles should all be on
+one side of the line, but a particular side is not guaranteed by this function. =#
+function _angles(::Type{T}, ::GI.LineStringTrait, geom) where T
+    npoints = GI.npoint(geom)
+    first_last_equal = equals(GI.getpoint(geom, 1), GI.getpoint(geom, npoints))
+    angle_list = Vector{T}(undef, npoints - (first_last_equal ? 1 : 2))
+    _find_angles!(
+        T, angle_list, geom;
+        offset = first_last_equal, close_geom = false,
+    )
+    return angle_list
+end
+
+#= The angles of a linearring are the angles within the closed line and include the angles
+formed by connecting the first and last points of the curve. =#
+function _angles(::Type{T}, ::GI.LinearRingTrait, geom; interior = true) where T
+    npoints = GI.npoint(geom)
+    first_last_equal = equals(GI.getpoint(geom, 1), GI.getpoint(geom, npoints))
+    angle_list = Vector{T}(undef, npoints - (first_last_equal ? 1 : 0))
+    _find_angles!(
+        T, angle_list, geom;
+        offset = true, close_geom = !first_last_equal, interior = interior,
+    )
+    return angle_list
+end
+
+#= The angles of a polygon is a vector of polygon angles. Note that if there are holes
+within the polyogn, the angles will be listed after the exterior ring angles in order of the
+holes. All angles, including the hole angles, are interior angles of the polygon.=#
+function _angles(::Type{T}, ::GI.PolygonTrait, geom) where T
+    angles = _angles(T, GI.LinearRingTrait(), GI.getexterior(geom); interior = true)
+    for h in GI.gethole(geom)
+        append!(angles, _angles(T, GI.LinearRingTrait(), h; interior = false))
+    end
+    return angles
+end
+
+#=
+Find angles of a curve and insert the values into the angle_list. If offset is true, then
+save space for the angle at the first vertex, as the curve is closed, at the front of
+angle_list. If close_geom is true, then despite the first and last point not being
+explicitly repeated, the curve is closed and the angle of the last point should be added to
+angle_list. If interior is true, then all angles will be on the same side of the line 
+=#
+function _find_angles!(
+    ::Type{T}, angle_list, geom;
+    offset, close_geom, interior = true,
+) where T
+    local p1, prev_p1_diff, p2_p1_diff
+    local start_point, start_diff
+    local extreem_idx, extreem_x, extreem_y
+    i_offset = offset ? 1 : 0
+    # Loop through the curve and find each of the angels
+    for (i, p2) in enumerate(GI.getpoint(geom))
+        xp2, yp2 = GI.x(p2), GI.y(p2)
+        #= Find point with smallest x values (and smallest y in case of a tie) as this point
+        is know to be convex. =#
+        if i == 1 || (xp2 < extreem_x || (xp2 == extreem_x && yp2 < extreem_y))
+            extreem_idx = i
+            extreem_x, extreem_y = xp2, yp2
+        end
+        if i > 1
+            p2_p1_diff = (xp2 - GI.x(p1), yp2 - GI.y(p1))
+            if i == 2
+                start_point = p1
+                start_diff = p2_p1_diff
+            else
+                angle_list[i - 2 + i_offset] = _diffs_calc_angle(T, prev_p1_diff, p2_p1_diff)
+            end
+            prev_p1_diff = -1 .* p2_p1_diff
+        end
+        p1 = p2
+    end
+    # If the last point of geometry should be the same as the first, calculate closing angle
+    if close_geom
+        p2_p1_diff = (GI.x(start_point) - GI.x(p1), GI.y(start_point) - GI.y(p1))
+        angle_list[end] = _diffs_calc_angle(T, prev_p1_diff, p2_p1_diff)
+        prev_p1_diff = -1 .* p2_p1_diff
+    end
+    # If needed, calculate first angle corresponding to the first point 
+    if offset
+        angle_list[1] = _diffs_calc_angle(T, prev_p1_diff, start_diff)
+    end
+    #= Make sure that all of the angles are on the same side of the line and inside of the
+    closed ring if the input geometry is closed. =#
+    inside_sgn = sign(angle_list[extreem_idx]) * (interior ? 1 : -1)
+    for i in eachindex(angle_list)
+        idx_sgn = sign(angle_list[i])
+        if idx_sgn == -1
+            angle_list[i] = abs(angle_list[i])
+        end
+        if idx_sgn != inside_sgn
+            angle_list[i] = 360 - angle_list[i]
+        end
+    end
+    return
+end
+
+#=
+Calculate the angle between two vectors defined by the previous and current Δx and Δys.
+Angle will have a sign corresponding to the sign of the cross product between the two
+vectors. All angles of one sign in a given geometry are convex, while those of the other
+sign are concave. However, the sign corresponding to each of these can vary based on
+geometry and thus you must compare to an angle that is know to be convex or concave.
+=#
+function _diffs_calc_angle(::Type{T}, (Δx_prev, Δy_prev), (Δx_curr, Δy_curr)) where T
+    cross_prod = Δx_prev * Δy_curr - Δy_prev * Δx_curr
+    dot_prod = Δx_prev * Δx_curr + Δy_prev * Δy_curr
+    prev_mag = max(sqrt(Δx_prev^2 + Δy_prev^2), eps(T))
+    curr_mag = max(sqrt(Δx_curr^2 + Δy_curr^2), eps(T))
+    val = clamp(dot_prod / (prev_mag * curr_mag), -one(T), one(T))
+    angle = real(acos(val) * 180 / π)
+    return angle * (cross_prod < 0 ? -1 : 1)
+end

src/methods/area.jl
@@ -13,20 +13,26 @@ negative area. The area is the absolute value of the signed area.
 
 To provide an example, consider this rectangle:
 ```@example rect
-using GeometryOps
-using GeometryOps.GeometryBasics
+import GeometryOps as GO
+import GeoInterface as GI
 using Makie
+using CairoMakie
 
-rect = Polygon([Point(0,0), Point(0,1), Point(1,1), Point(1,0), Point(0, 0)])
-f, a, p = poly(rect; axis = (; aspect = DataAspect()))
+rect = GI.Polygon([[(0,0), (0,1), (1,1), (1,0), (0, 0)]])
+f, a, p = poly(collect(GI.getpoint(rect)); axis = (; aspect = DataAspect()))
 ```
 This is clearly a rectangle, etc.  But now let's look at how the points look:
 ```@example rect
-lines!(a, rect; color = 1:length(coordinates(rect))+1)
+lines!(
+    collect(GI.getpoint(rect));
+    color = 1:GI.npoint(rect), linewidth = 10.0)
 f
 ```
-The points are ordered in a clockwise fashion, which means that the signed area
+The points are ordered in a counterclockwise fashion, which means that the signed area
 is negative.  If we reverse the order of the points, we get a postive area.
+```@example rect
+GO.signed_area(rect)  # -1.0
+```
 
 ## Implementation
 
@@ -49,7 +55,7 @@ const _AREA_TARGETS = Union{GI.PolygonTrait,GI.AbstractCurveTrait,GI.MultiPointT
     area(geom, ::Type{T} = Float64)::T
 
 Returns the area of a geometry or collection of geometries. 
-This is computed slighly differently for different geometries:
+This is computed slightly differently for different geometries:
 
     - The area of a point/multipoint is always zero.
     - The area of a curve/multicurve is always zero.

src/methods/centroid.jl
@@ -14,21 +14,18 @@ centroids are calculated by weighting edge's by their 'area components'.
 
 To provide an example, consider this concave polygon in the shape of a 'C':
 ```@example cshape
-using GeometryOps
-using GeometryOps.GeometryBasics
+import GeometryOps as GO
+import GeoInterface as GI
 using Makie
 using CairoMakie
 
-cshape = Polygon([
-    Point(0,0), Point(0,3), Point(3,3), Point(3,2), Point(1,2),
-    Point(1,1), Point(3,1), Point(3,0), Point(0,0),
-])
-f, a, p = poly(cshape; axis = (; aspect = DataAspect()))
+cshape = GI.Polygon([[(0,0), (0,3), (3,3), (3,2), (1,2), (1,1), (3,1), (3,0), (0,0)]])
+f, a, p = poly(collect(GI.getpoint(cshape)); axis = (; aspect = DataAspect()))
 ```
 Let's see what the centroid looks like (plotted in red):
 ```@example cshape
-cent = centroid(cshape)
-scatter!(a, GI.x(cent), GI.y(cent), color = :red)
+cent = GO.centroid(cshape)
+scatter!(GI.x(cent), GI.y(cent), color = :red)
 f
 ```
 

commit 791b83103d0034719bab056c939f7d1036d4c74b

Make Documenter warnings errors, and fix everything that surfaces
`warnonly = true` has hidden a growing pile of broken docs since #50.  Turn
every Documenter check into an error and fix the 18 problems that were being
swallowed.

Broken `@example` blocks:

- `spatial_joins.md` had a trailing comma in `using FlexiJoins,`, so the whole
  setup block failed to parse and all four later blocks on the page ran with
  nothing imported.
- `geospatial_geometry.md` passed a whole `DataFrame` to `poly!` (no recipe),
  and called `DisplayAs.setcontext(y, ...)` on a `y` that is named `ys`.
- `cap.jl` did `using GeometryOps`, which does not bring `UnitSphericalPoint`
  or `SphericalCap` into scope - they live in `GeometryOps.UnitSpherical`.
- `RobustCrossProduct.jl` used `⋅` without importing it.
- `smooth.jl` called `GeographicFromUnitSpherical`, which has never existed;
  the transform is `GeographicFromUnitSphere`.
- `segmentize.jl`'s benchmark died in `plot_trials`, not in GEOS: current Makie
  shares one `alpha` between `ScatterLines` and its child `Lines`, so setting
  `alpha` on the child throws.  Fade the line through `color` instead and pin
  the markers back with `markercolor`.

Missing and duplicated docstrings:

- `barycentric_coordinates`, `barycentric_coordinates!` and
  `barycentric_interpolate` are exported but had no docstrings at all, so their
  `@docs` entries in `api.md` rendered nothing.
- `circumcenter_on_unit_sphere` was listed in a `@docs` block without a
  docstring.
- `reproject` and `applyreduce` carry their docstrings on bare `function f end`
  declarations, so they land under the `Union{}` signature and collide with the
  `@docs` entries in `api.md`.  Exclude `reproject` from the catch-all
  `@autodocs`, and mark the `applyreduce.jl` block `canonical=false` the way
  `apply.jl` already is.

Stray Julia interpolation:

- Two commented-out lines in `apply.jl`/`applyreduce.jl` become Literate

GeometryOpsCore/src/apply.jl
@@ -364,7 +364,7 @@ end
 # Fail loudly if we hit PointTrait without running `f`
 # (after PointTrait there is no further to dig with `_apply`)
 # @inline _apply(f, ::TraitTarget{Target}, trait::GI.PointTrait, geom; crs=nothing, kw...) where Target =
-    # throw(ArgumentError("target $Target not found, but reached a `PointTrait` leaf"))
+    # throw(ArgumentError("target \$Target not found, but reached a `PointTrait` leaf"))
 # Finally, these short methods are the main purpose of `apply`.
 # The `Trait` is a subtype of the `Target` (or identical to it)
 # So the `Target` is found. We apply `f` to geom and return it to previous

GeometryOpsCore/src/applyreduce.jl
@@ -39,7 +39,7 @@ and perform some operation on it.
 [`centroid`](@ref), [`area`](@ref) and [`distance`](@ref) have been implemented using the 
 [`applyreduce`](@ref) framework.
 
-```@docs
+```@docs; canonical=false
 applyreduce
 ```
 
@@ -160,7 +160,7 @@ end
 end
 # Fail if we hit PointTrait
 # _applyreduce(f, op, target::TraitTarget{Target}, trait::PointTrait, geom; kw...) where Target = 
-    # throw(ArgumentError("target $target not found"))
+    # throw(ArgumentError("target \$target not found"))
 # Specific cases to avoid method ambiguity
 for T in (
     GI.PointTrait, GI.LinearRing, GI.LineString, 

benchmarks/benchmark_plots.jl
@@ -112,7 +112,14 @@ function plot_trials(
             ygridwidth = 0.75,
         ) 
         plots = [scatterlines!(ax, x, y; label = label) for (x, y, label) in zip(xs, ys, labels)]
-        setproperty!.(getindex.(getproperty.(plots, :plots), 1), :alpha, 0.1)
+        # De-emphasize the connecting lines while keeping the markers opaque.  `ScatterLines`
+        # shares a single `alpha` with its child `Lines` plot, so fade the line via `color`
+        # and pin the markers back to the cycled colour with `markercolor`.
+        for plot in plots
+            color = Makie.to_color(plot.color[])
+            plot.markercolor = color
+            plot.color = Makie.RGBAf(color.r, color.g, color.b, 0.1)
+        end
         leg = Legend(
             lp, ax; 
             tellwidth = legend_position isa Union{Tuple, Makie.Automatic} && (legend_position isa Makie.Automatic || length(legend_position) != 3) && legend_orientation == :vertical, 

docs/Project.toml
@@ -19,7 +19,6 @@ ExactPredicates = "429591f6-91af-11e9-00e2-59fbe8cec110"
 Extents = "411431e0-e8b7-467b-b5e0-f676ba4f2910"
 FlexiJoins = "e37f2e79-19fa-4eb7-8510-b63b51fe0a37"
 GADM = "a8dd9ffe-31dc-4cf5-a379-ea69100a8233"
-GLMakie = "e9467ef8-e4e7-5192-8a1a-b1aee30e663a"
 GeoDataFrames = "62cb38b5-d8d2-4862-a48e-6a340996859f"
 GeoDatasets = "ddc7317b-88db-5cb5-a849-8449e5df04f9"
 GeoFormatTypes = "68eda718-8dee-11e9-39e7-89f7f65f511f"

docs/make.jl
@@ -156,7 +156,12 @@ makedocs(;
         ],
         "Source code" => literate_pages,
     ],
-    warnonly = true,
+    # Every Documenter check is an error, so that broken examples, dead cross-references
+    # and duplicated docstrings fail CI instead of silently degrading the built docs.
+    # `:missing_docs` is still only a warning: the 58 docstrings it flags all live in
+    # internal submodules (`UnitSpherical`, `SpatialTreeInterface`, `FlexibleRTrees`,
+    # `NaturalIndexing`, `LoopStateMachine`) which have no API page yet.
+    warnonly = [:missing_docs],
 )
 
 DocumenterVitepress.deploydocs(;

src/methods/barycentric.jl
@@ -230,6 +230,21 @@ function t_value(sᵢ::_VecTypes{N, T1}, sᵢ₊₁::_VecTypes{N, T1}, rᵢ::T2,
 end
 
 
+"""
+    barycentric_coordinates(method, geom, point; normalize = true)
+
+Return the barycentric coordinates of `point` with respect to the vertices of `geom`,
+as a newly allocated `Vector` holding one weight per vertex.
+
+`method` is an [`AbstractBarycentricCoordinateMethod`](@ref), currently always
+[`MeanValue`](@ref).  `geom` may be any GeoInterface-compatible curve (a linear ring or
+linestring) with at least three points, and `point` any GeoInterface-compatible point.
+
+If `normalize` is `true` (the default) the returned weights are scaled to sum to 1.
+
+See also [`barycentric_coordinates!`](@ref), which writes into a preallocated vector, and
+[`barycentric_interpolate`](@ref), which uses these weights to interpolate values.
+"""
 function barycentric_coordinates(alg::AbstractBarycentricCoordinateMethod, geom, in_point; normalize = true)
     barycentric_coordinates(alg, GI.geomtrait(geom), geom, GI.geomtrait(in_point), in_point; normalize)
 end
@@ -240,6 +255,15 @@ function barycentric_coordinates(alg::AbstractBarycentricCoordinateMethod, t1::G
     return λs
 end
 
+"""
+    barycentric_coordinates!(λs, method, geom, point; normalize = true)
+
+Write the barycentric coordinates of `point` with respect to the vertices of `geom` into
+`λs`, and return `λs`.
+
+This is the allocation-free form of [`barycentric_coordinates`](@ref); `λs` must have one
+element per vertex of `geom`.
+"""
 function barycentric_coordinates!(λs::Vector{<: Real}, ::AbstractBarycentricCoordinateMethod, geom, in_point; normalize = true)
     @boundscheck @assert GI.npoint(geom) >= 3
     barycentric_coordinates!(λs, MeanValue(), GI.geomtrait(geom), geom, GI.geomtrait(in_point), in_point; normalize)
@@ -315,6 +339,20 @@ end
 # end
 # ```
 
+"""
+    barycentric_interpolate(method, geom, values, point)
+
+Interpolate `values`, given one per vertex of `geom`, to `point` using barycentric
+coordinates, and return the interpolated value.
+
+`geom` may be a curve (linear ring or linestring) or a polygon; for polygons the values
+are taken around the exterior ring first and then around each hole, in the order
+`GeoInterface.getring` returns them.  `values` may be numbers or, for the mean value
+method, colors.
+
+This is equivalent to weighting `values` by [`barycentric_coordinates`](@ref), but
+accumulates in place and so allocates less.
+"""
 function barycentric_interpolate(alg::AbstractBarycentricCoordinateMethod, geom, values::AbstractVector, point)
     barycentric_interpolate(alg, GI.geomtrait(geom), geom, values, GI.geomtrait(point), point)
 end

src/transformations/smooth.jl
@@ -29,8 +29,8 @@ using CairoMakie
 import GeoInterface as GI, GeometryOps as GO
 
 line = GI.LineString([(0.0, 0.0), (1.0, 1.0), (2.0, 0.0)])
-smoothed = GO.smooth(GO.Spherical(), line) |> x -> GO.transform(GO.UnitSpherical.GeographicFromUnitSpherical(), x)
-smoothed_2 = GO.smooth(GO.Spherical(), line; iterations=2) |> x -> GO.transform(GO.UnitSpherical.GeographicFromUnitSpherical(), x)
+smoothed = GO.smooth(GO.Spherical(), line) |> x -> GO.transform(GO.UnitSpherical.GeographicFromUnitSphere(), x)
+smoothed_2 = GO.smooth(GO.Spherical(), line; iterations=2) |> x -> GO.transform(GO.UnitSpherical.GeographicFromUnitSphere(), x)
 
 f, a, p = lines(line; label = "Original", axis = (; title = "Spherical smoothing"))
 lines!(a, smoothed; label = "1 iteration")

src/utils/UnitSpherical/cap.jl
@@ -29,7 +29,7 @@ The `SphericalCap` type offers multiple constructors to create caps from:
 ## Examples
 
 ```@example sphericalcap
-using GeometryOps
+using GeometryOps.UnitSpherical
 using GeoInterface
 
 # Create a spherical cap from a point and radius
@@ -159,6 +159,15 @@ function _merge(x::SphericalCap, y::SphericalCap)
     end
 end
 
+"""
+    circumcenter_on_unit_sphere(a, b, c)
+
+Return the center of the circle passing through the three `UnitSphericalPoint`s
+`a`, `b` and `c`, as a `UnitSphericalPoint`.
+
+Of the two antipodal circumcenters, this returns the one on the same hemisphere as the
+input points, i.e. the center of the *smaller* of the two circles they bound.
+"""
 function circumcenter_on_unit_sphere(a::UnitSphericalPoint, b::UnitSphericalPoint, c::UnitSphericalPoint)
     raw = LinearAlgebra.cross(a, b) +
           LinearAlgebra.cross(b, c) +

src/utils/UnitSpherical/robustcrossproduct/RobustCrossProduct.jl
@@ -27,6 +27,7 @@ This implementation handles these edge cases by:
 
 ```@example robust-cross
 using GeometryOps.UnitSpherical
+using LinearAlgebra: ⋅
 
 # Regular case - points at right angles
 a = UnitSphericalPoint(1, 0, 0)